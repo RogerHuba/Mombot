3b8e59c27be550976dbf5cb1e7c648eb3340ded8
Steven <steven@entropy.cc>
2018-12-08 05:39:12 -0600

    Adding limit to select output

--- Commands/Data/select.ts
+++ Commands/Data/select.ts
@@ -349,7 +349,7 @@ else
 	if ($count > 1000)
 		setVar $SWITCHBOARD~message "Displaying results for: select "&$original_query&"* *Your query returned "&$count&" results.*This is too many to display on subspace. *If you'd like to narrow your search, add more parameters.*All result sectors are now marked with QUERY sector parameter.*You can also display individual results with the sector bot command.*"
 	else
-		setVar $SWITCHBOARD~message $results&"* *Your query returned "&$count&" results.*All result sectors are now marked with QUERY sector parameter.*You can also display individual results with the sector bot command.*"
+		setVar $SWITCHBOARD~message "Displaying results for: select "&$original_query&"* *"&$results&"* *Your query returned "&$count&" results.*All result sectors are now marked with QUERY sector parameter.*You can also display individual results with the sector bot command.*"
 	end
 end
 gosub :SWITCHBOARD~switchboard
