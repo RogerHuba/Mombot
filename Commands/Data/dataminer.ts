488c048da8d8e0dd817d0957f80267a4e5dcd6b4
Steven <steven@entropy.cc>
2018-12-09 22:57:13 -0600

    Removing version externsions

--- Commands/Data/dataminer.ts
+++ Commands/Data/dataminer.ts
@@ -14,16 +14,13 @@
 	setVar $BOT~help[1]  $BOT~tab&"Dataminer - Grabs Game Information.  "
 	setVar $BOT~help[2]  $BOT~tab&"       "
 	setVar $BOT~help[3]  $BOT~tab&"Dataminer Options: "
-	setVar $BOT~help[4]  $BOT~tab&"     [deadend] - Create text file for deadends / missing deadend"
-	setVar $BOT~help[5]  $BOT~tab&"     [blister] - will strip fuel off planets"
-	setVar $BOT~help[6]  $BOT~tab&"      [tunnel] - Create text file for tunnels and missing tunnels"
-	setVar $BOT~help[7]  $BOT~tab&"     [traffic] - Create traffic analysis file"
-	setVar $BOT~help[8]  $BOT~tab&"        [2way] - Create text file for 2-way secotors"
-	setVar $BOT~help[9]  $BOT~tab&"        [3way] - Create text file for 3-way secotors"
-	setVar $BOT~help[10]  $BOT~tab&"        [4way] - Create text file for 4-way secotors"
-	setVar $BOT~help[11]  $BOT~tab&"        [5way] - Create text file for 5-way secotors"
-	setVar $BOT~help[12]  $BOT~tab&"        [6way] - Create text file for 6-way secotors"
-	setVar $BOT~help[13]  $BOT~tab&"    [subspace] - Displays data over SS (NOT IMPLEMENTED)"
+	setVar $BOT~help[4]  $BOT~tab&"     [map] - Create text file for all sectors"
+	setVar $BOT~help[5]  $BOT~tab&"           - deadends, 2way, 3way..."
+	setVar $BOT~help[6]  $BOT~tab&"    [port] - Create all port reports"
+	setVar $BOT~help[7]  $BOT~tab&" [blister] - looks for blister (U bubbles)"
+	setVar $BOT~help[8]  $BOT~tab&"  [tunnel] - Create text file for tunnels and missing tunnels"
+	setVar $BOT~help[9]  $BOT~tab&" [traffic] - Create traffic analysis file"
+	setVar $BOT~help[10]  $BOT~tab&"     [ss] - Displays data over SS (NOT IMPLEMENTED)"
 
         gosub :BOT~help_file
         setVar $BOT~script_title "Dataminer"
@@ -35,47 +32,32 @@
         gosub :PLAYER~quikstats
 
         setvar $path "dataminer\" & GAMENAME & "-"
-        getWordPos $user_command_line $pos "deadend"
+        getWordPos $user_command_line $pos "map"
     	if ($pos > 0)
              delete $path & deadend.txt
 	     delete $path & missingdeadends.txt
-             gosub :makedeadend
-             gosub :makemissingdeadends
-        end
-        getWordPos $user_command_line $pos "2way"
-    	if ($pos > 0)
              delete $path & 2way.txt
 	     delete $path & missing2way.txt
-             gosub :make2way
-             gosub :makemissing2way
-        end
-        getWordPos $user_command_line $pos "3way"
-    	if ($pos > 0)
              delete $path & 3way.txt
 	     delete $path & missing3way.txt
-             gosub :make3way
-             gosub :makemissing3way
-        end
-        getWordPos $user_command_line $pos "4way"
-    	if ($pos > 0)
              delete $path & 4way.txt
 	     delete $path & missing4way.txt
-             gosub :make4way
-             gosub :makemissing4way
-        end
-        getWordPos $user_command_line $pos "5way"
-    	if ($pos > 0)
              delete $path & 5way.txt
 	     delete $path & missing5way.txt
-             gosub :make5way
-             gosub :makemissing5way
-        end
-        getWordPos $user_command_line $pos "6way"
-    	if ($pos > 0)
              delete $path & 6way.txt
 	     delete $path & missing6way.txt
-             gosub :make6way
-             gosub :makemissing6way
+             delete $path & 7way.txt
+	     delete $path & missing7way.txt
+             gosub :makedeadend
+
+        end
+        getWordPos $user_command_line $pos "port"
+	if ($pos > 0)
+	     delete $path & sellers.txt
+	     delete $path & buyers.txt
+             delete $path & fuel.txt
+             gosub :tunnelfind
+             gosub :makeMissingTunnels
         end
         getWordPos $user_command_line $pos "tunnel"
 	if ($pos > 0)
@@ -122,7 +104,6 @@ while ($count <= SECTORS)
      end
      add $count 1
 end
-return
 
 :makemissingdeadends
 setvar $count 1
@@ -134,7 +115,6 @@ while ($count <= SECTORS)
      end
 	add $count 1
 end
-return
 
 :make2way
 setArray $2way SECTORS
@@ -143,7 +123,7 @@ while ($count <= SECTORS)
      if (SECTOR.WARPINCOUNT[$count] = 2)
           setvar $2way[$count] 1
 	  add $2way 1
-	  if (SECTOR.WARPCOUNT[$count] > 1)
+	  if (SECTOR.WARPCOUNT[$count] >= 1)
 	       write $path & 2way.txt $count & " Has " & SECTOR.WARPCOUNT[$count] & " ways out."
                     setSectorParameter $count "2WAY" TRUE
           else
@@ -154,7 +134,6 @@ while ($count <= SECTORS)
      end
      add $count 1
 end
-return
 
 :makemissing2way
 setvar $count 1
@@ -166,7 +145,6 @@ while ($count <= SECTORS)
      end
 	add $count 1
 end
-return
 
 :make3way
 setArray $3way SECTORS
@@ -175,7 +153,7 @@ while ($count <= SECTORS)
      if (SECTOR.WARPINCOUNT[$count] = 3)
           setvar $3way[$count] 1
 	  add $3way 1
-	  if (SECTOR.WARPCOUNT[$count] > 1)
+	  if (SECTOR.WARPCOUNT[$count] >= 1)
 	       write $path & 3way.txt $count & " Has " & SECTOR.WARPCOUNT[$count] & " ways out."
                     setSectorParameter $count "3WAY" TRUE
           else
@@ -187,7 +165,6 @@ while ($count <= SECTORS)
      end
      add $count 1
 end
-return
 
 :makemissing3way
 setvar $count 1
@@ -199,7 +176,6 @@ while ($count <= SECTORS)
      end
 	add $count 1
 end
-return
 
 :make4way
 setArray $4way SECTORS
@@ -208,7 +184,7 @@ while ($count <= SECTORS)
      if (SECTOR.WARPINCOUNT[$count] = 4)
           setvar $4way[$count] 1
 	  add $4way 1
-	  if (SECTOR.WARPCOUNT[$count] > 1)
+	  if (SECTOR.WARPCOUNT[$count] >= 1)
 	       write $path & 4way.txt $count & " Has " & SECTOR.WARPCOUNT[$count] & " ways out."
                     setSectorParameter $count "4WAY" TRUE
           else
@@ -219,7 +195,6 @@ while ($count <= SECTORS)
       end
      add $count 1
 end
-return
 
 :makemissing4way
 setvar $count 1
@@ -231,7 +206,6 @@ while ($count <= SECTORS)
      end
 	add $count 1
 end
-return
 
 :make5way
 setArray $5way SECTORS
@@ -240,7 +214,7 @@ while ($count <= SECTORS)
      if (SECTOR.WARPINCOUNT[$count] = 5)
           setvar $5way[$count] 1
 	  add $5way 1
-	  if (SECTOR.WARPCOUNT[$count] > 1)
+	  if (SECTOR.WARPCOUNT[$count] >= 1)
 	       write $path & 5way.txt $count & " Has " & SECTOR.WARPCOUNT[$count] & " ways out."
                     setSectorParameter $count "5WAY" TRUE
           else
@@ -251,7 +225,6 @@ while ($count <= SECTORS)
      end
      add $count 1
 end
-return
 
 :makemissing5way
 setvar $count 1
@@ -263,7 +236,6 @@ while ($count <= SECTORS)
      end
 	add $count 1
 end
-return
 
 :make6way
 setArray $6way SECTORS
@@ -272,7 +244,7 @@ while ($count <= SECTORS)
      if (SECTOR.WARPINCOUNT[$count] = 6)
           setvar $6way[$count] 1
 	  add $6way 1
-	  if (SECTOR.WARPCOUNT[$count] > 1)
+	  if (SECTOR.WARPCOUNT[$count] >= 1)
 	       write $path & 6way.txt $count & " Has " & SECTOR.WARPCOUNT[$count] & " ways out."
                     setSectorParameter $count "6WAY" TRUE
           else
@@ -283,7 +255,6 @@ while ($count <= SECTORS)
      end
      add $count 1
 end
-return
 
 :makemissing6way
 setvar $count 1
@@ -295,8 +266,37 @@ while ($count <= SECTORS)
      end
 	add $count 1
 end
-return
 
+:make7way
+setArray $7way SECTORS
+setvar $count 1
+while ($count <= SECTORS)
+     if (SECTOR.WARPINCOUNT[$count] = 7)
+          setvar $7way[$count] 1
+	  add $6way 1
+	  if (SECTOR.WARPCOUNT[$count] >= 1)
+	       write $path & 7way.txt $count & " Has " & SECTOR.WARPCOUNT[$count] & " ways out."
+                    setSectorParameter $count "7WAY" TRUE
+          else
+	       write $path & 7way.txt $count
+	  end
+     else
+          setSectorParameter $count "7WAY" ""
+     end
+     add $count 1
+end
+
+:makemissing7way
+setvar $count 1
+while ($count <= SECTORS)
+     if (SECTOR.WARPINCOUNT[$count] = 7)
+          if ($figsec[$count] = 0)
+               write $path & missing7way.txt $count
+          end
+     end
+	add $count 1
+end
+return
 :tunnelfind
 setVar $twoWarpSectors 0
 setArray $twoWarps 0
