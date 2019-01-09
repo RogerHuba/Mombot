85c67ebfb5517b902afc71a9a71db196bc65b91b
Steven <steven@entropy.cc>
2019-01-09 01:21:23 -0600

    updated mac

--- commands/general/mac.ts
+++ commands/general/mac.ts
@@ -44,6 +44,7 @@
     stripText $user_command_line $SWITCHBOARD~bot_name
     StripText $user_command_line " mac "
     replaceText $user_command_line "^m" "*"
+    replaceText $user_command_line "^b" #8
     replaceText $user_command_line #42 "*"
     getWordPos $user_command_line $pos "`"
     getWordPos $user_command_line $pos2 "'"
