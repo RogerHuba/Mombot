8ce96666dec0b0b12970e7671e765d0d8300fec0
Steven <steven@entropy.cc>
2019-06-26 15:32:20 -0500

    clear

--- source/module_includes/modules.ts
+++ source/module_includes/modules.ts
@@ -1,9 +1,8 @@
 :clear
     gosub :PLAYER~QUIKSTATS
     setVar $startingLocation $PLAYER~CURRENT_PROMPT
-    setVar $PROMPT~startingLocation $PLAYER~CURRENT_PROMPT
-    setVar $PROMPT~validPrompts "Command Citadel"
-    gosub :PROMPT~checkStartingPrompt
+    setVar $bot~validPrompts "Command Citadel"
+    gosub :bot~checkStartingPrompt
 
     if ($startingLocation = "Citadel")
         send "q"
