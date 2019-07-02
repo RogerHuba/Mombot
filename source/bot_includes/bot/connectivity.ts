416d85cdd89b0b3bcd1f721d2399c590a7f2d1ab
Steven <steven@entropy.cc>
2019-07-02 10:20:56 -0500

    remove space tabs

--- source/bot_includes/bot/connectivity.ts
+++ source/bot_includes/bot/connectivity.ts
@@ -1,24 +1,24 @@
 #===================================== KEEP ALIVE ============================================
 :keepalive
-        send #27
-        setvar $relog_message ""
-        add $alive_count 1
-    if ($alive_count >= ($BOT~echoInterval * 2))
-        setVar $alive_count 0
-        gosub :PLAYER~current_prompt
-        getSectorParameter 2 "FIG_COUNT" $BOT~figCount
-        echo ANSI_14 "*-= Time: " ANSI_15 TIME ANSI_14 " Fig Grid: " ANSI_15 $BOT~figCount ANSI_14 " =-*" ANSI_7
-        echo CURRENTANSILINE
-    end
-    if ((CONNECTED <> TRUE) AND ($BOT~doRelog = TRUE))
+		send #27
+		setvar $relog_message ""
+		add $alive_count 1
+	if ($alive_count >= ($BOT~echoInterval * 2))
+		setVar $alive_count 0
+		gosub :PLAYER~current_prompt
+		getSectorParameter 2 "FIG_COUNT" $BOT~figCount
+		echo ANSI_14 "*-= Time: " ANSI_15 TIME ANSI_14 " Fig Grid: " ANSI_15 $BOT~figCount ANSI_14 " =-*" ANSI_7
+		echo CURRENTANSILINE
+	end
+	if ((CONNECTED <> TRUE) AND ($BOT~doRelog = TRUE))
 		if ($relogging <> true)
 			setvar $relogging true
 			savevar $relogging
 			goto :internal_commands~relog_attempt
 		end
-    end
-    
-    # if the last line hasn't changed for the last two keep alive checks #
+	end
+	
+	# if the last line hasn't changed for the last two keep alive checks #
 	if ($last_prompt_seen = CURRENTLINE)
 		# at server game menu for some reason #
 		if ((CURRENTLINE = $game~game_menu_prompt) or (CURRENTLINE = "[Pause] - [Press Space or Enter to continue]") or (CURRENTLINE = "Enter your choice: ") or (CURRENTLINE = "Selection (? for menu): "))
@@ -32,90 +32,90 @@
 		end
 		# TODO - add checking for subprompt interactivity turned off and resetting prompts that turn off comms if stuck there #
 	end
-    setvar $last_prompt_seen CURRENTLINE
-    send #27
-    setDelayTrigger     keepalive               :keepalive           30000
-    pause
+	setvar $last_prompt_seen CURRENTLINE
+	send #27
+	setDelayTrigger     keepalive               :keepalive           30000
+	pause
 #=================================== END KEEP ALIVE ==========================================
 
 
 
 #================================= ONLINE WATCH/RELOG ========================================
 :online_watch
-    if ((CONNECTED <> TRUE) AND ($BOT~doRelog = TRUE))
+	if ((CONNECTED <> TRUE) AND ($BOT~doRelog = TRUE))
 		if ($relogging <> true)
 			setvar $relogging true
 			savevar $relogging
 			goto :internal_commands~relog_attempt
 		end
-    end
-    setTextTrigger      online_watch            :online_watch              "Your session will be terminated in "
-    send #27
-    pause
+	end
+	setTextTrigger      online_watch            :online_watch              "Your session will be terminated in "
+	send #27
+	pause
 
 
 
 :do_relog
-        :thedelay
-            gosub :killrelogtriggers
-            setEventTrigger continuelogin :continuelogin "CONNECTION ACCEPTED"
-            if (CONNECTED <> TRUE)
-                ECHO "*"&ansi_15&"["&ansi_3&"ATTEMPTING TO CONNECT"&ansi_15&"]*"
-                connect
-            else
-            	goto :continueRelog3
-            end
-            pause
-            :continuelogin
-            gosub :killrelogtriggers
-            setTextTrigger relog3 :continueRelog3 "Please enter your name"
-            pause
-        :continueRelog3
-            gosub :killrelogtriggers
-            setTextTrigger loginsuccessful :continueRelog4 "Trade Wars 2002"
-            setTextTrigger loginsuccessful2 :continueRelog4 "Copyright (C) EIS"
-            send $BOT~username & "*"
-            pause
+		:thedelay
+			gosub :killrelogtriggers
+			setEventTrigger continuelogin :continuelogin "CONNECTION ACCEPTED"
+			if (CONNECTED <> TRUE)
+				ECHO "*"&ansi_15&"["&ansi_3&"ATTEMPTING TO CONNECT"&ansi_15&"]*"
+				connect
+			else
+				goto :continueRelog3
+			end
+			pause
+			:continuelogin
+			gosub :killrelogtriggers
+			setTextTrigger relog3 :continueRelog3 "Please enter your name"
+			pause
+		:continueRelog3
+			gosub :killrelogtriggers
+			setTextTrigger loginsuccessful :continueRelog4 "Trade Wars 2002"
+			setTextTrigger loginsuccessful2 :continueRelog4 "Copyright (C) EIS"
+			send $BOT~username & "*"
+			pause
 
-        :continueRelog4
-            gosub :killrelogtriggers
-            if ($first_time)
-                setVar $first_time FALSE
-                disconnect
-                goto :do_relog
-            end
-            setTextTrigger relog69 :continueRelog5 "Make a Selection:"
-            setTextTrigger relog3 :continueRelog5 "Selection (? for menu):"
-            send "#"&#8
-            pause
-        :continueRelog5
-            gosub :killrelogtriggers
-            setTextTrigger firstpause :firstpause "[Pause]"
-            setTextTrigger enter :done_do_relog "Enter your choice"
-            send $BOT~letter
-            pause
-        :firstpause
-            send "*"
-            setTextTrigger firstpause :firstpause "[Pause]"
-            pause
-        :done_do_relog
-            killalltriggers
+		:continueRelog4
+			gosub :killrelogtriggers
+			if ($first_time)
+				setVar $first_time FALSE
+				disconnect
+				goto :do_relog
+			end
+			setTextTrigger relog69 :continueRelog5 "Make a Selection:"
+			setTextTrigger relog3 :continueRelog5 "Selection (? for menu):"
+			send "#"&#8
+			pause
+		:continueRelog5
+			gosub :killrelogtriggers
+			setTextTrigger firstpause :firstpause "[Pause]"
+			setTextTrigger enter :done_do_relog "Enter your choice"
+			send $BOT~letter
+			pause
+		:firstpause
+			send "*"
+			setTextTrigger firstpause :firstpause "[Pause]"
+			pause
+		:done_do_relog
+			killalltriggers
 return
 
 :killrelogtriggers
-    killtrigger continuelogin
-    killtrigger thedelay
-    killtrigger thedelay2
-    killtrigger relog
-    killtrigger relog2
-    killtrigger relog3
-    killtrigger relog69
-    killtrigger relog89
-    killtrigger loginsuccessful
-    killtrigger loginsuccessful2
-    killtrigger firstpause
-    killtrigger enter
-    setDelayTrigger thedelay2 :thedelay 5000
+	killtrigger continuelogin
+	killtrigger thedelay
+	killtrigger thedelay2
+	killtrigger relog
+	killtrigger relog2
+	killtrigger relog3
+	killtrigger relog69
+	killtrigger relog89
+	killtrigger loginsuccessful
+	killtrigger loginsuccessful2
+	killtrigger firstpause
+	killtrigger enter
+	setDelayTrigger thedelay2 :thedelay 5000
 return
 
 :enter_new_game 
@@ -144,7 +144,7 @@ return
 	:closed
 		killalltriggers
 		if (CONNECTED <> TRUE)
-		    load "scripts\mombot\commands\general\relog.cts"
+			load "scripts\mombot\commands\general\relog.cts"
 			setEventTrigger		1		:relogended	"SCRIPT STOPPED" "scripts\mombot\commands\general\relog.cts"
 			pause
 			:relogended
@@ -233,10 +233,10 @@ return
 					setVar $menus~mowDestination $MAP~stardock
 				end
 			end
-		    setvar $BOT~user_command_line "mow "&$menus~mowDestination&" "
-		    setVar $BOT~parm1 $menus~mowDestination
+			setvar $BOT~user_command_line "mow "&$menus~mowDestination&" "
+			setVar $BOT~parm1 $menus~mowDestination
 			savevar $bot~user_command_line
-		    savevar $bot~parm1
+			savevar $bot~parm1
 			load "scripts\mombot\modes\grid\mow.cts"
 			setEventTrigger		1		:mowended	"SCRIPT STOPPED" "scripts\mombot\modes\grid\mow.cts"
 			pause
