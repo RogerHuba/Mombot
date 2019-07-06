cca8553e267101340ed2ce7b44708d3a5ef0c5fc
Hamish Borthen <hamish@boeschcomputing.com.au>
2019-07-07 07:25:42 +0800

    Added passive grid aliens to lspassgrid

--- modes/grid/lspassgrid.ts
+++ modes/grid/lspassgrid.ts
@@ -270,7 +270,35 @@
 		setVar $restock 1
 	end
 
-	
+
+	getWordPos $bot~user_command_line $pos "reset"
+	if ($pos > 0)
+
+		setvar $r 11
+		while ($r <= SECTORS)
+			setSectorParameter $r "LSCHK" FALSE
+			add $r 1
+		end
+	else
+		setvar $r 11
+		while ($r <= SECTORS)
+			getSectorParameter $r "LSCHK" $lschk
+			if ($lschk = TRUE)
+				setVar $CHKD[$r] 1
+			else
+				setVar $CHKD[$r] 0
+			end
+			add $r 1
+		end
+		
+	end
+
+	setVar $ignorea 0
+	getWordPos $bot~user_command_line $pos "ignorea"
+	if ($pos > 0)
+		setVar $ignorea 1
+	end
+
 	if ($FILTER_DENSITY = 1)
 		goSub :getPersonalPlanets
 	end
@@ -449,15 +477,19 @@
 					end
 				end
 			end
-		setVar $i 1
+		
 		setArray $Adj_Targets SECTOR.WARPCOUNT[$player~CURRENT_SECTOR]
 		setArray $Filtered_Density SECTOR.WARPCOUNT[$player~CURRENT_SECTOR]
 
+		setVar $holoRequired 0
+		setVar $firstFilter 1
+
+
+		:refilter
+		setVar $i 1
 		while ($i <= SECTOR.WARPCOUNT[$player~CURRENT_SECTOR])
-        		setVar $adj SECTOR.WARPS[$player~CURRENT_SECTOR][$i]
-			setVar $Adj_Targets[$i] 10
+			setVar $adj SECTOR.WARPS[$player~CURRENT_SECTOR][$i]
 			setVar $currentDensity SECTOR.DENSITY[$adj]
-			
 			if ($FILTER_DENSITY = 1)
 				if ($planetsInSectors[$adj] > 0)
 					subtract $currentDensity (500 * $planetsInSectors[$adj])
@@ -471,6 +503,55 @@
 				end
 					
 			end
+			if ($ignorea = 1)
+				if ($firstFilter = 1)
+					getSectorParameter $adj "FIGSEC" $Flag
+					isNumber $tst $Flag
+					if ($tst = 0)
+						setVar $Flag 0
+						setSectorParameter $adj "FIGSEC" FALSE
+					end
+					if (($flag = 0) and (($currentDensity <> 0) and $currentDensity <> 100))
+						# not our fig there, and density not passive
+						setVar $holoRequired 1
+					end
+				else
+					setVar $figsowner SECTOR.FIGS.OWNER[$adj]
+					getWordPos $figsowner $whereowner "belong to"
+		
+					if ($whereowner = 0)
+						if (SECTOR.FIGS.QUANTITY[$adj] < $player~FIGHTERS)
+							subtract $currentDensity (SECTOR.FIGS.QUANTITY[$adj] * 5)
+						end
+					end
+					
+
+				end
+				
+			end
+
+			setVar $Filtered_Density[$i] $currentDensity
+			add $i 1
+		end 
+
+		if ($holoRequired = 1)
+			setVar $firstfilter 0
+			setVar $holoRequired 0
+			send "zn"
+			waitfor "o you want instructions (Y/N) [N]?"
+			gosub :Do_Holo
+	
+
+			goto :refilter
+			
+		end
+
+		setVar $i 1
+		while ($i <= SECTOR.WARPCOUNT[$player~CURRENT_SECTOR])
+        		setVar $adj SECTOR.WARPS[$player~CURRENT_SECTOR][$i]
+			setVar $Adj_Targets[$i] 10
+			setVar $currentDensity $Filtered_Density[$i]
+			
 			if (SECTOR.NAVHAZ[$adj] <> 0)
 				setVar $filter 0
 				setVar $Filter (SECTOR.NAVHAZ[$adj] * 21)
@@ -1435,7 +1516,7 @@
 	return
 :Do_Holo
 	setArray $HoloOutput 2000
-    setVar $Line_Pointer 1
+	setVar $Line_Pointer 1
                 	send "SzH*  "
 	setTextLineTrigger	TurnsGone		:TurnsGone		"Do you want instructions (Y/N) [N]?"
 	setTextLineTrigger	DoneScan		:DoneScan		"Warps to Sector(s) :"
