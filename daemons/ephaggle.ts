ab6077d81da07bbbf4e7edbd979e4c9243903f2b
Hamish Borthen <hamish@boeschcomputing.com.au>
2019-07-08 09:44:48 +0800

    Debug Off

--- daemons/ephaggle.ts
+++ daemons/ephaggle.ts
@@ -4,8 +4,8 @@ systemscript
 
 // Brought to you by Shadow's CTS Decompiler 2.0
 
-setvar $verbose_debug_mode true
-setvar $paused_debug_mode true
+setvar $verbose_debug_mode false
+setvar $paused_debug_mode false
 setvar $VERSION 2019
 setvar $SIGLINE "EP's Perfect Haggle, v. " & $VERSION
 
