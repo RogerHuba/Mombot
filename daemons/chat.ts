4f35967fde7f6d1824450c47810394e45c58d56b
Steven <steven@entropy.cc>
2019-07-25 13:03:10 -0500

    Updating scripts

--- daemons/chat.ts
+++ daemons/chat.ts
@@ -34,5 +34,5 @@ pause
 include "source\module_includes\bot\loadvars\bot"
 include "source\module_includes\bot\helpfile\bot"
 include "source\module_includes\bot\banner\bot"
-include "source\module_includes\switchboard"
+include "source\bot_includes\switchboard"
 
