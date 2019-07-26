df7bd8e4f5a57d8125670dbe370636eabadc1e95
Steven <steven@entropy.cc>
2019-07-26 14:19:02 -0500

    updating tab-?

--- source/bot_includes/bot/user_interface.ts
+++ source/bot_includes/bot/user_interface.ts
@@ -811,7 +811,8 @@ return
 			setVar $i 10
 			goto :runHotScript
 		elseif ($charCode = "63")
-			goto :HELP~echo_help
+			setVar $BOT~user_command_line "help"
+			goto :runUserCommandLine
 		elseif (($charCode >= 49) AND ($charCode <= 57))
 			setVar $i ($charCode-48)
 			goto :runHotScript
