66747236ca8b34943d156d76d9761b07e972a0f4
Steven <steven@entropy.cc>
2019-07-29 16:22:49 -0500

    adding help functionality

--- source/bot_includes/bot/user_interface.ts
+++ source/bot_includes/bot/user_interface.ts
@@ -577,9 +577,12 @@ return
 		setVar $BOT~command "help"
 	end
 	if (($bot~command = "help") and ($bot~parm1 <> ""))
-		setvar $bot~command $bot~parm1
-		setvar $bot~parm1 "help"
-		setvar $bot~user_command_line $bot~command&" "&$bot~parm1
+		fileExists $doesExist "scripts\mombot\help\"&$BOT~parm1&".txt"
+		if ($doesExist <> true)
+			setvar $bot~command $bot~parm1
+			setvar $bot~parm1 "help"
+			setvar $bot~user_command_line $bot~command&" "&$bot~parm1
+		end
 	end
 
 	if ($BOT~command = "build") or ($BOT~command = "create")
