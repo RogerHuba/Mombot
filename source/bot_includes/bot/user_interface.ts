ade2a59beba6f8cd845b1e099872a1eb84c79609
Steven <steven@entropy.cc>
2019-07-29 16:43:56 -0500

    update

--- source/bot_includes/bot/user_interface.ts
+++ source/bot_includes/bot/user_interface.ts
@@ -584,7 +584,8 @@ return
 			setvar $bot~user_command_line $bot~command&" "&$bot~parm1
 		end
 	end
-	
+	echo "**[["&$bot~user_command_line&"]]**"
+	halt
 	if ($BOT~command = "build") or ($BOT~command = "create")
 		setVar $BOT~command $bot~parms[1]
 		setvar $bot~parms[1] "create"
