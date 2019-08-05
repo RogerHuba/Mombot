928d290091653c7e7c01d7dd259c299354baaf1c
Steven <steven@entropy.cc>
2019-08-05 00:14:12 -0500

    adding public hkill

--- daemons/sidecar.ts
+++ daemons/sidecar.ts
@@ -167,11 +167,7 @@
 		pause
 		:howmany
 			getword currentline $mycount 3
-			setvar $line currentline
-			echo "[["&$line&"]]"
-			replacetext $line "You have "&$mycount&" fighters, and "&user_name&" has " ""
-			replacetext $line "." ""
-			setvar $current_fighters $line
+			getText CURRENTLINE $current_fighters "You have "&$mycount&" fighters, and "&$user_name&" has " "."
 			echo "*[["&$current_fighters&"]]*"
 			pause
 		:toomany
