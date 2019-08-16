8d5f1ef1a31a908d72df546bcb44612d2160ff7a
Steven <steven@entropy.cc>
2019-08-16 16:10:17 -0500

    updated bg2019

--- modes/cashing/wppt.ts
+++ modes/cashing/wppt.ts
@@ -129,5 +129,6 @@ include "source\module_includes\bot\loadvars\bot"
 include "source\module_includes\bot\helpfile\bot"
 include "source\module_includes\bot\banner\bot"
 include "source\bot_includes\player\isephaggle\player"
+include "source\bot_includes\player\quikstats\player"
 include "source\module_includes\wppt\worldTrade"
 
