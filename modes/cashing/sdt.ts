6d399cb4ea94080ab5f4a55117fdc6d73dd78ac9
Hamish Borthen <hamish@boeschcomputing.com.au>
2019-08-18 20:34:24 +0800

    Send bug in sdt fix

--- modes/cashing/sdt.ts
+++ modes/cashing/sdt.ts
@@ -77,7 +77,7 @@ reqRecording
 
     if ($GAME~steal_factor = 0)
 	setVar $GAME~steal_factor 21
-	send "No Steal factor!! assuming 21, you need to ensure bot has refreshed!*"
+	send "'{" $switchboard~bot_name "}No Steal factor!! assuming 21, you need to ensure bot has refreshed!*"
     end
 
 	gosub :player~isEpHaggle
