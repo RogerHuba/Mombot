5cdf66aa8827cb1623e377572a2a6099f8b51d45
Steven Ruzicka <steven@entropy.cc>
2019-08-22 19:28:38 -0500

    updated

--- modes/grid/mowthis.ts
+++ modes/grid/mowthis.ts
@@ -696,6 +696,8 @@ return
 
 
 #-=-=-=-=-includes-=-=-=-=-
+include "source\module_includes\bot\loadvars\bot"
+include "source\module_includes\bot\helpfile\bot"
 include "source\bot_includes\player\quikstats\player"
 include "source\bot_includes\player\twarp\player"
 include "source\bot_includes\player\currentprompt\player"
