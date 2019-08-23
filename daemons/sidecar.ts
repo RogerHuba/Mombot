9c09a4e8c42fdc0c9c5697221c8cd847a91f73ed
Steven <steven@entropy.cc>
2019-08-23 14:46:09 -0500

    drop

--- daemons/sidecar.ts
+++ daemons/sidecar.ts
@@ -96,11 +96,13 @@
 	pause
 
 
+
 	:validate_tow
-	getwordpos currentansiline $pos  "[1;36m"
-	getwordpos currentansiline $pos " [0;32mlocks a tractor beam on your ship."
+	setvar $line currentansiline
+	getwordpos $line $pos  "[K[1;36m"
+	getwordpos $line $pos " [0;32mlocks a tractor beam on your ship."
 	if (($pos > 0) and ($pos2 > 0))
-		getText CURRENTANSILINE $user_name "[1;36m" " [0;32mlocks a tractor beam on your ship."
+		getText $line $user_name "[K[1;36m" " [0;32mlocks a tractor beam on your ship."
 		setvar $switchboard~message "Sidecar attached to "&$user_name&"'s ship.*"
 		gosub :switchboard~switchboard
 
