431082fe7ca5938f3c9e7842eda77f8cab6ab147
Steven <steven@entropy.cc>
2019-08-30 15:17:16 -0500

    team mega

--- source/public/TWXP/scripts/mombot/move_public.bat
+++ source/public/TWXP/scripts/mombot/move_public.bat
@@ -63,6 +63,7 @@ copy "..\..\..\..\..\commands\resource\_dock_shopper.cts" "commands\resource\_do
 copy "..\..\..\..\..\commands\cashing\mega.cts" "commands\cashing\mega.cts"
 copy "..\..\..\..\..\commands\cashing\tricon.ts" "commands\cashing\tricon.cts"
 copy "..\..\..\..\..\commands\general\tow.cts" "commands\general\tow.cts"
+copy "..\..\..\..\..\commands\grid\port.cts" "commands\grid\port.cts"
 copy "..\..\..\..\..\commands\general\help.cts" "commands\general\help.cts"
 copy "..\..\..\..\..\commands\cashing\neg.cts" "commands\cashing\neg.cts"
 copy "..\..\..\..\..\commands\cashing\rob.cts" "commands\cashing\rob.cts"
