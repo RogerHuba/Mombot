45fa7f9eb676c726b88f67d11a330608a372c0fd
Steven <steven@entropy.cc>
2019-09-06 23:53:19 -0500

    team mega

--- commands/general/help.ts
+++ commands/general/help.ts
@@ -124,7 +124,7 @@
 			end
 			gosub :bufferList
 		end
-		setVar $SWITCHBOARD~message $SWITCHBOARD~message&"  ---------------------------------- *"
+		setVar $SWITCHBOARD~message $SWITCHBOARD~message&"  *  ---------------------------------- *"
 	end
 	#if ($SWITCHBOARD~self_command <> TRUE)
 	if (($SWITCHBOARD~self_command = true) or ($BOT~silent_running = TRUE))
