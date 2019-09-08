a1859cd781d13604d4a9693e89d8e7aba6a342a8
Steven Ruzicka <steven@entropy.cc>
2019-09-07 23:51:01 -0500

    param

--- modes/data/param.ts
+++ modes/data/param.ts
@@ -105,5 +105,4 @@ return
 
 # includes:
 include "source\module_includes\bot\loadvars\bot"
-include "source\bot_includes\switchboard"
-include "source\bot_includes\player\currentprompt\player"
+include "source\module_includes\bot\helpfile\bot"
