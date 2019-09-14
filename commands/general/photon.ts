f0d2fc57a333a6a01e4c1e782909627fed69d84b
Steven <steven@entropy.cc>
2019-09-13 22:01:57 -0500

    updating

--- commands/general/photon.ts
+++ commands/general/photon.ts
@@ -53,5 +53,6 @@ halt
 
 #INCLUDES:
 include "source\module_includes\bot\loadvars\bot"
+include "source\module_includes\bot\checkstartingprompt\bot"
 include "source\module_includes\bot\helpfile\bot"
 include "source\bot_includes\player\quikstats\player"
