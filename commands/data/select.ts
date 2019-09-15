6f333bff80255401fd814d821cf6bdbf6da47351
Steven <steven@entropy.cc>
2019-09-15 16:20:38 -0500

    updating

--- commands/data/select.ts
+++ commands/data/select.ts
@@ -700,9 +700,9 @@ if ($count <= 0)
 	setVar $SWITCHBOARD~message "Displaying results for: select "&$original_query&"* *Your query returned "&$count&" results.*"
 else
 	if ($count > 1000)
-		setVar $SWITCHBOARD~message "Displaying results for: select "&$original_query&"* *Your query returned "&$count&" results.*This is too many to display on subspace. *If you'd like to narrow your search, add more parameters.*All result sectors are now marked with QUERY sector parameter.*You can also display individual results with the sector bot command.*"
+		setVar $SWITCHBOARD~message $limit&"]Displaying results for: select "&$original_query&"* *Your query returned "&$count&" results.*This is too many to display on subspace. *If you'd like to narrow your search, add more parameters.*All result sectors are now marked with QUERY sector parameter.*You can also display individual results with the sector bot command.*"
 	else
-		setVar $SWITCHBOARD~message "Displaying results for: select "&$original_query&"* *"&$results&"* *Your query returned "&$count&" results.*All result sectors are now marked with QUERY sector parameter.*You can also display individual results with the sector bot command.*"
+		setVar $SWITCHBOARD~message $limit&"]Displaying results for: select "&$original_query&"* *"&$results&"* *Your query returned "&$count&" results.*All result sectors are now marked with QUERY sector parameter.*You can also display individual results with the sector bot command.*"
 	end
 end
 gosub :SWITCHBOARD~switchboard
