d26abb085a20c605a86b5c5d847c2711eb7c4498
Steven <steven@entropy.cc>
2019-09-23 00:46:56 -0500

    updating

--- daemons/alienhunt.ts
+++ daemons/alienhunt.ts
@@ -664,26 +664,25 @@ return
 	if (($PLAYER~TURNS > 0) and ($player~unlimitedGame <> true))
 			setVar $msg $msg & "* Turns: " & $PLAYER~TURNS
 	end
-	setarray $window 7
-	setvar $window 7
-	setvar $window[1] "** Alienhunt Planet: " & $planet~planet
-	setvar $window[2] "* -----------------------------------------------"
+	setarray $window_lines 7
+	setvar $window_lines[1] "** Alienhunt Planet: " & $planet~planet
+	setvar $window_lines[2] "* -----------------------------------------------"
 	format $planet~planet_fuel $player~value NUMBER
-	setvar $window[3] "* Planet Fuel: " & $player~value&"                          "
-	cutText $window[3] $window[3] 1 30
+	setvar $window_lines[3] "* Planet Fuel: " & $player~value&"                          "
+	cutText $window_lines[3] $window_lines[3] 1 30
 	format $planet~planet_fighters $player~value NUMBER
-	setvar $window[4] "   Planet Fighters: " & $player~value
+	setvar $window_lines[4] "   Planet Fighters: " & $player~value
 	format $planet~planet_shields $player~value NUMBER
-	setvar $window[5] "* Planet Shields: " & $player~value&"                          "
-	cutText $window[5] $window[5] 1 30
+	setvar $window_lines[5] "* Planet Shields: " & $player~value&"                          "
+	cutText $window_lines[5] $window_lines[5] 1 30
 	format $planet~citadel_credits $player~value NUMBER
-	setvar $window[6] "   Citadel Credits: " & $player~value
+	setvar $window_lines[6] "   Citadel Credits: " & $player~value
 	format $player~fighters $player~value NUMBER
-	setvar $window[7] "* Ship Fighters: " & $player~value&"*"
+	setvar $window_lines[7] "* Ship Fighters: " & $player~value&"*"
 
 	setvar $i 1
-	while ($i >= $window)
-		setvar $msg $msg&$window[$i]
+	while ($i >= 7)
+		setvar $msg $msg&$window_lines[$i]
 		add $i 1
 	end
 	setWindowContents alienhunt_script $msg 
