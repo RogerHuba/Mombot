ee881bd136fe45fae9964ae33162dc00d0c130f1
Steven <steven@entropy.cc>
2019-09-23 00:32:40 -0500

    updating

--- daemons/alienhunt.ts
+++ daemons/alienhunt.ts
@@ -686,7 +686,7 @@ return
 	setvar $new_window[5] 20
 	setvar $new_window[6] 20
 	setvar $new_window[7] 20
-	cutLengths $window $new_window  20
+	cutLengths $window 20
 pause
 	setvar $i 1
 	while ($i >= $window)
