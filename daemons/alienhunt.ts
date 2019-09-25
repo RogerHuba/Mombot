7b78a43900c07ad5728a88a27c22ee5514e74192
Steven <steven@entropy.cc>
2019-09-24 21:22:57 -0500

    updating

--- daemons/alienhunt.ts
+++ daemons/alienhunt.ts
@@ -582,7 +582,9 @@ return
 				setvar $is_fuel_buyer PORT.BUYFUEL[$startingSector]
 				setvar $is_port PORT.EXISTS[$startingSector]
 				setvar $class PORT.CLASS[$startingSector]
-				if (($refuel = true) and ($is_fuel_buyer <> true) and ($is_port = true) and ($class > 0))
+				getSectorParameter $PLAYER~CURRENT_SECTOR "BUSTED" $isBusted
+
+				if (($refuel = true) and ($is_fuel_buyer <> true) and ($is_port = true) and ($class > 0) and ($isBusted <> true))
 					if ($upgrade = true)
 						killAllTriggers
 						send "q"
