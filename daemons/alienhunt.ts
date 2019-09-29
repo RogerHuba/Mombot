238ba08ffeaa557dc040a74a0eb7a02ebfc6b8f6
Steven <steven@entropy.cc>
2019-09-29 13:59:03 -0500

    update

--- daemons/alienhunt.ts
+++ daemons/alienhunt.ts
@@ -552,7 +552,7 @@ return
 		getSectorParameter currentsector "BUSTED" $isBusted
 		getSectorParameter currentsector "UPGRADEF" $isUpgradedFuel
 
-		if (($refuel = true) and ($is_fuel_buyer <> true) and ($is_port = true) and ($class > 0) and ($isBusted <> true) and ($under_construction <> true) and ($planet~planetfuel < ($planet~planetfuelmax-$game~port_max))
+		if (($refuel = true) and ($is_fuel_buyer <> true) and ($is_port = true) and ($class > 0) and ($isBusted <> true) and ($under_construction <> true) and ($planet~planet_fuel < ($planet~planet_fuel_max-$game~port_max))
 			if (($upgrade = true) and ($isUpgradedFuel <> true))
 				gosub :max~run
 				gosub :setwindow
