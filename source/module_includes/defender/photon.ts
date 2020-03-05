580676390a1f3d81b539041749aaecc916fb6f4b
Steven <steven@entropy.cc>
2020-03-05 00:58:32 -0600

    up

--- source/module_includes/defender/photon.ts
+++ source/module_includes/defender/photon.ts
@@ -26,7 +26,7 @@
 	end
 
 :fire_photon
-	send "p" $adjsec "*  y  c  p  y  " $sector "**q"
+	send "p" $adjsec "*  y  c  p  y  " $sector "**qp" $sector "*  y  "
 	setTextLineTrigger	1	:photon_missed	      "That is not an adjacent sector"
 	setTextLineTrigger	2	:photon_gotem	      "Photon Missile launched into sector"
 	setTextLineTrigger	3	:photon_fed 	      "The Feds do not permit Photon Torpedos"
@@ -65,6 +65,13 @@
 	gosub :killtriggers
 	setvar $switchboard~message "Photon Fired - Sector => " & $sector & "!*"
 	gosub :switchboard~switchboard
+	gosub :player~quikstats
+	###################################
+	# if direct drop worked, do htorp #
+	###################################
+	if ($player~current_sector = $sector)
+		gosub :htorp
+	end
 	if ($density = true)
 		gosub :densityDrop
 	end
@@ -185,5 +192,9 @@ return
 
 :retreatphoton
 	send "p" $sector "*  y  "
+	gosub :htorp
+return
+
+:htorp
 	gosub :htorp~run
 return
