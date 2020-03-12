b0a7704cbe352e7cda40dca44ffdf189e03f9f19
Steven <steven@entropy.cc>
2020-03-12 00:43:50 -0500

    fix for cap

--- modes/offense/dockkill.ts
+++ modes/offense/dockkill.ts
@@ -5,7 +5,10 @@
 	setVar $BOT~help[1] $BOT~tab&"Scans for targets and autokills in sector."
 	setVar $BOT~help[2] $BOT~tab&"         "
 	setVar $BOT~help[3] $BOT~tab&"Options: "
-	setVar $BOT~help[4] $BOT~tab&"{off} - Turns off script and sets planet and ship corporate."
+	setVar $BOT~help[4] $BOT~tab&"   {off} - Turns off script "
+	setVar $BOT~help[5] $BOT~tab&"   {pod} - Only shoots pods"
+	setVar $BOT~help[6] $BOT~tab&"  {meat} - meatgrinder mode"
+	setVar $BOT~help[7] $BOT~tab&"   {cap} - capture instead of kill"
 	gosub :bot~helpfile
 
 	setVar $BOT~script_title "Dock Killer"
