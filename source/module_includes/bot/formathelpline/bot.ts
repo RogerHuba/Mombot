e6daa6af39c130bcc5738993d89e9fe1a7fc6f64
Steven <steven@entropy.cc>
2020-03-13 00:29:52 -0500

    Updating deploy

--- source/module_includes/bot/formathelpline/bot.ts
+++ source/module_includes/bot/formathelpline/bot.ts
@@ -2,6 +2,7 @@
 
 	replaceText $line "[" ansi_2&"["&ansi_6
 	replaceText $line "]" ansi_2&"]"&ansi_13
+	replaceText $line ">" ansi_2&">"&ansi_14
 	replaceText $line "-" ansi_7&"-"&ansi_13
 	replaceText $line "<<<<" ansi_14&"<"&ansi_7&"<"&ansi_14&"<"&ansi_7&"<"&ansi_15
 	replaceText $line ">>>>" ansi_7&">"&ansi_14&">"&ansi_7&">"&ansi_14&">"
