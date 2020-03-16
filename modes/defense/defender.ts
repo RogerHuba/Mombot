98c59f156e55bd4b36e3ace07cbc54873608358f
Steven <steven@entropy.cc>
2020-03-15 21:50:38 -0500

    removing duplicates

--- modes/defense/defender.ts
+++ modes/defense/defender.ts
@@ -291,7 +291,7 @@
 	else
 		setVar $message $message&"*                   Deploy mines: No"
 	end
-	setVar $message $message&"*      Home Sector: "&$map~home_sector
+	setVar $message $message&"*                    Home Sector: "&$map~home_sector
 	format $planet~planet_Fighters $formatted_fighters NUMBER
 	setVar $message $message&"*        Auto Kill: Enabled With "&$formatted_fighters&" Fighters"
 	setVar $message $message&"*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-**"	
