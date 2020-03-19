06775e5de4ff6a1da58d0e26ff0ca27c4d8f069f
Steven <steven@entropy.cc>
2020-03-19 01:23:15 -0500

    removing duplicates

--- commands/data/history.ts
+++ commands/data/history.ts
@@ -1,10 +1,12 @@
 	logging off
 	gosub :BOT~loadVars
 
-	setVar $BOT~help[1]  $BOT~tab&" history "
+	setVar $BOT~help[1]  $BOT~tab&" history {limit#}"
 	setVar $BOT~help[2]  $BOT~tab&"   "
 	setVar $BOT~help[3]  $BOT~tab&"     Displays the most recent self commands"
 	setVar $BOT~help[4]  $BOT~tab&"     this bot has given."
+	setVar $BOT~help[5]  $BOT~tab&"         "
+	setVar $BOT~help[6]  $BOT~tab&"     {limit#} - display the last # of commands"
 
 	gosub :bot~helpfile
 
