6e09fa69301fed3d0b680f0432f75a187548ee81
Steven <steven@entropy.cc>
2020-03-19 01:18:09 -0500

    removing duplicates

--- commands/data/history.ts
+++ commands/data/history.ts
@@ -17,6 +17,8 @@
 	loadVar $BOT~historyString
 	setVar $BOT~historyCount 0
 
+
+
 	setvar $switchboard~message ""
 
 	getWordPos $BOT~historyString $pos "<<|HS|>>"
@@ -28,12 +30,28 @@
 		getWordPos $BOT~historyString $pos "<<|HS|>>"
 	end
 
+	isNumber $isnumber $bot~parm1
+	setvar $default false
+	if ($isnumber = true)
+		setvar $history_limit $bot~parm1
+	else
+			setvar $switchboard~message "History limit must be a number*"
+			gosub :switchboard~message
+			halt
+	end
+	if ($history_limit = 0)
+		setvar $history_limit $BOT~historyCount
+	end
+
 	setvar $i $BOT~historyCount
-	while ($i >= 1)
+	setvar $count 0
+	setvar $switchboard~message $switchboard~message&"Displaying last "&history_limit&" commands in self command prompt:*"
+	while (($i >= 1) and ($count <= $history_limit))
 		if ($history[$i] <> "0")
 			setvar $switchboard~message $switchboard~message&$history[$i]&"*"
 		end
 		subtract $i 1
+		add $count 1
 	end
 
 	gosub :switchboard~switchboard
