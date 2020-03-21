0e93c06d01bcbb65b4b73b760b29a6be47ba79b8
Steven <steven@entropy.cc>
2020-03-21 02:14:35 -0500

    removing duplicates

--- modes/defense/saveme.ts
+++ modes/defense/saveme.ts
@@ -637,4 +637,5 @@ return
 include "source\module_includes\bot\loadvars\bot"
 include "source\module_includes\bot\helpfile\bot"
 include "source\bot_includes\player\quikstats\player"
+include "source\bot_includes\ship\getshipstats\ship"
 include "source\bot_includes\planet\getplanetinfo\planet"
