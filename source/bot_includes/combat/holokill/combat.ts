b271e7c698de67c10e6f7494a57ff466fa7a03a0
Steven <steven@entropy.cc>
2020-03-22 06:18:00 -0500

    removing duplicates

--- source/bot_includes/combat/holokill/combat.ts
+++ source/bot_includes/combat/holokill/combat.ts
@@ -116,6 +116,7 @@
 			gosub :player~quikstats
 			if ($player~current_sector <> $hkill_start_sector)
 				gosub :callsaveme
+				setVar $SWITCHBOARD~message "After save me, resetting.*"
 			else
 				setVar $SWITCHBOARD~message "Attack made and back in original sector!*"
 			end
@@ -157,7 +158,8 @@
 			end
 			gosub :player~quikstats
 			if ($player~current_sector <> $hkill_start_sector)
-				   gosub :callsaveme
+				gosub :callsaveme
+				setVar $SWITCHBOARD~message "After save me, resetting.*"
 			else
 				setVar $SWITCHBOARD~message "Attack made and back in original sector!*"
 			end
