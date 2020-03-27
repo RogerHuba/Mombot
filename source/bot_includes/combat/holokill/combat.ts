e4e51b0f1ede67702b3a8a99c0b023b19000cdcc
Steven <steven@entropy.cc>
2020-03-26 21:50:23 -0500

    Test

--- source/bot_includes/combat/holokill/combat.ts
+++ source/bot_includes/combat/holokill/combat.ts
@@ -178,5 +178,6 @@
 return
 
 include "source\bot_includes\player\currentprompt\player"
+include "source\bot_includes\sector\getautosectordata\sector"
 include "source\bot_includes\grid\surround\grid"
 include "source\bot_includes\ship\getshipstats\ship"
