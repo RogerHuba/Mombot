0910fb5674b7f894f46b26734b9285e1b6dc88f6
Steven <steven@entropy.cc>
2020-03-26 21:15:52 -0500

    Test

--- source/bot_includes/combat/holokill/combat.ts
+++ source/bot_includes/combat/holokill/combat.ts
@@ -28,51 +28,40 @@
 		return
 :holo_kill_scandone
 		killalltriggers
-		waiton "[" & $player~current_sector & "]"
+		gosub :sector~getAutoSectorData
 
 :holo_kill_get_prompt
 :holo_kill_get_current_sector
-		setVar $hkill_start_sector $player~current_sector
+		setVar $hkill_start_sector $sector~starting_sector
+		setvar $player~current_sector $starting_sector
 		setVar $killsector 0
-		setVar $idx 1
-		while ($idx <= SECTOR.WARPCOUNT[$player~current_sector])
-			setVar $test_sector SECTOR.WARPS[$player~current_sector][$idx]
-			setVar $safePlanets TRUE
-			setVar $containsShieldedPlanet FALSE
-			setvar $containsEnemyTrader FALSE
-			if (SECTOR.PLANETCOUNT[$test_sector] > 0)
-				setVar $p 1
-				while ($p <= SECTOR.PLANETCOUNT[$test_sector])
-					getWord SECTOR.PLANETS[$test_sector][$p] $test 1
-					if ($test = "<<<<")
-						setVar $containsShieldedPlanet TRUE
-					end
-					add $p 1
+
+		setVar $test_sector $sector~targetSector
+		setVar $safePlanets TRUE
+		setVar $containsShieldedPlanet FALSE
+		setvar $containsEnemyTrader FALSE
+		if (SECTOR.PLANETCOUNT[$test_sector] > 0)
+			setVar $p 1
+			while ($p <= SECTOR.PLANETCOUNT[$test_sector])
+				getWord SECTOR.PLANETS[$test_sector][$p] $test 1
+				if ($test = "<<<<")
+					setVar $containsShieldedPlanet TRUE
 				end
-				if ($player~surroundAvoidAllPlanets)
-					setVar $safePlanets FALSE
-				elseif (($containsShieldedPlanet) AND ($player~surroundAvoidShieldedOnly))
-					setVar $safePlanets FALSE
-				end
+				add $p 1
 			end
-			if (SECTOR.TRADERCOUNT[$test_sector] > 0)
-				setvar $t 1
-				while ($t <= SECTOR.TRADERCOUNT[$test_sector])
-					getwordpos SECTOR.TRADERS[$test_sector][$t] $pos "["&$player~corp&"]"
-					if ($pos <= 0)
-						setVar $containsEnemyTrader TRUE
-					end
-					add $t 1
-				end
+			if ($player~surroundAvoidAllPlanets)
+				setVar $safePlanets FALSE
+			elseif (($containsShieldedPlanet) AND ($player~surroundAvoidShieldedOnly))
+				setVar $safePlanets FALSE
 			end
-			setVar $figowner SECTOR.FIGS.OWNER[$test_sector]
-			if (($test_sector <> $MAP~stardock) AND ($test_sector > 10) AND (SECTOR.TRADERCOUNT[$test_sector] > 0) AND ($containsEnemyTrader = TRUE) AND ($safePlanets = TRUE) and ((SECTOR.FIGS.QUANTITY[$test_sector] < ($too_many_fighters*2)) OR (($figOwner = "belong to your Corp") OR ($figOwner = "yours"))))
-				setVar $killsector $test_sector
-				goto :holo_kill_killem
-			end
-			add $idx 1
 		end
-		goto :holo_kill_no_targets
+		setVar $figowner SECTOR.FIGS.OWNER[$test_sector]
+		if (($test_sector <> $MAP~stardock) AND ($test_sector > 10) AND ($safePlanets = TRUE) and ((SECTOR.FIGS.QUANTITY[$test_sector] < ($too_many_fighters*2)) OR (($figOwner = "belong to your Corp") OR ($figOwner = "yours"))))
+			setVar $killsector $test_sector
+		else
+			setVar $SWITCHBOARD~message "Cannot holokill - check for planets or too many figs?*"
+			return
+		end
 
 :holo_kill_killem
 		if ($slingshot)
@@ -81,7 +70,7 @@
 			setvar $title "Holokill"
 		end
 		if ($noavoid <> true)
-			send "'{" $SWITCHBOARD~bot_name "} - " $title " - Attacking sector "  $test_sector  ".*   c v 0 * y n "  $test_sector  " *  q  "
+			 send "c v 0 * y n "  $test_sector  " *  q  "
 		end
 		if ($slingshot)
 			if ($player~cit = true)
@@ -104,7 +93,6 @@
 				return
 			end
 			send "m * * * c "
-			goSub :SECTOR~getSectorData
 			if ($holocapture)
 				gosub :combat~fastCapture
 				send " l " $PLANET~PLANET " * n n * j m * * * j c  *  "
@@ -118,17 +106,18 @@
 				gosub :callsaveme
 				setVar $SWITCHBOARD~message "After save me, resetting.*"
 			else
-				setVar $SWITCHBOARD~message "Attack made and back in original sector!*"
+				setVar $SWITCHBOARD~message  $title&" - Attacking sector"&$test_sector&".*"
+				setVar $SWITCHBOARD~message $SWITCHBOARD~message&"Attack made and back in original sector!*"
 			end
 		else
 			if ($player~cit = true)
 				if ($switch)
-					send " e y q m * * * q  m z "  $test_sector  " *  *  z  a  " $SHIP~SHIP_MAX_ATTACK "*  z  a  " $SHIP~SHIP_MAX_ATTACK "*  R  *  f  z  1  *  z  c  d  *   "
+					send " e y q m * * * q  m z "  $test_sector  " *  *  z  a  " $SHIP~SHIP_MAX_ATTACK "*  z  a  " $SHIP~SHIP_MAX_ATTACK "*  R  *  "
 				else
-					send " q m * * * q  m z "  $test_sector  " *  *  z  a  " $SHIP~SHIP_MAX_ATTACK "*  z  a  " $SHIP~SHIP_MAX_ATTACK "*  R  *  f  z  1  *  z  c  d  *   "
+					send " q m * * * q  m z "  $test_sector  " *  *  z  a  " $SHIP~SHIP_MAX_ATTACK "*  z  a  " $SHIP~SHIP_MAX_ATTACK "*  R  *  "
 				end
 			else
-				send " m z "  $test_sector  " *  *  z  a  " $SHIP~SHIP_MAX_ATTACK "*  z  a  " $SHIP~SHIP_MAX_ATTACK "*  R  *  f  z  1  *  z  c  d  *   "
+				send " m z "  $test_sector  " *  *  z  a  " $SHIP~SHIP_MAX_ATTACK "*  z  a  " $SHIP~SHIP_MAX_ATTACK "*  R  *  "
 			end
 			if ($player~surround_before_hkill = TRUE)
 				gosub :player~quikstats
@@ -137,24 +126,19 @@
 				gosub :player~quikstats
 			end
 		
-			gosub  :player~quikstats
-			if ($player~current_prompt <> "Command")
-				setVar $SWITCHBOARD~message "Wrong prompt for holokill kill.*"
-				return
-			end
-			setvar $PLAYER~startingLocation "Command"
-			goSub :SECTOR~getSectorData
 			if ($holocapture)
 				gosub :combat~fastCapture
 			else
 				goSub :fastAttack
 			end		
-			send "m "  $hkill_start_sector  " *  *  z  a  99999  *  z  a  99999  *  R  *   "
 			if ($player~CIT = TRUE)
-				send " l "  $PLANET~PLANET  " * n n * j m * * * j c  *  "
 				if ($switch)
-					send " e y "
+					send "  f  z  1  *  z  c  d  *   m "  $hkill_start_sector  " *  *  z  a  99999  *  z  a  99999  *  R  *    l "  $PLANET~PLANET  " * n n * j m * * * j c  *   e y "					
+				else
+					send "  f  z  1  *  z  c  d  *   m "  $hkill_start_sector  " *  *  z  a  99999  *  z  a  99999  *  R  *    l "  $PLANET~PLANET  " * n n * j m * * * j c  *  "
 				end
+			else
+				send "  f  z  1  *  z  c  d  *   m "  $hkill_start_sector  " *  *  z  a  99999  *  z  a  99999  *  R  *   "
 			end
 			gosub :player~quikstats
 			if ($player~current_sector <> $hkill_start_sector)
@@ -162,15 +146,12 @@
 				gosub :player~quikstats
 				setVar $SWITCHBOARD~message "After save me, resetting.*"
 			else
-				setVar $SWITCHBOARD~message "Attack made and back in original sector!*"
+				setVar $SWITCHBOARD~message  $title&" - Attacking sector"&$test_sector&".*"
+				setVar $SWITCHBOARD~message $SWITCHBOARD~message&"Attack made and back in original sector!*"
 			end
 
 		end
 	return
-:holo_kill_no_targets
-		setVar $SWITCHBOARD~self_command TRUE
-		setVar $SWITCHBOARD~message "No Enemies found adjacent!*"
-return
 
 
 :callsaveme
