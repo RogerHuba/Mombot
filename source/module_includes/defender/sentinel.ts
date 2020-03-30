2f6571af0def6ac0cb9db60394131a979c2d9585
Steven <steven@entropy.cc>
2020-03-30 11:24:21 -0500

    defender

--- source/module_includes/defender/sentinel.ts
+++ source/module_includes/defender/sentinel.ts
@@ -6,9 +6,7 @@
 	gosub :switchboard~switchboard
 	setvar $i 1
 	if ($corp_count > 0)
-		echo "[1;44m                               Who's Playing                                 
-[0m"
-
+		echo "[1;44m                               Who's Playing                                 [0m"
 		echo ansi_14 "*                 Corp Info                   " 
 		echo "*   Name                           Sector" ansi_7 "*------------------------------------------" 
 		while ($i <= $corp_count)
