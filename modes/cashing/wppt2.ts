a09097d8666bcc38fb97758015ea5e769f1ba851
Steven <steven@entropy.cc>
2020-04-03 16:00:26 -0500

    wppt2

--- modes/cashing/wppt2.ts
+++ modes/cashing/wppt2.ts
@@ -1620,6 +1620,191 @@ return
 	halt
 
 return
+
+
+
+:DoTwarp
+	setVar $msg ""
+	if ($warpto > 0)
+		send "q q * * mz" & $warpto "*"
+		setTextTrigger there        :adj_warp "You are already in that sector!"
+		setTextLineTrigger adj_warp :adj_warp "Sector  : " & $warpto & " "
+		setTextTrigger locking      :locking "Do you want to engage the TransWarp drive?"
+		setTextTrigger igd          :twarpIgd "An Interdictor Generator in this sector holds you fast!"
+		setTextTrigger noturns      :twarpPhotoned "Your ship was hit by a Photon and has been disabled"
+		setTextTrigger noroute      :twarpNoRoute "Do you really want to warp there? (Y/N)"
+		pause
+		:adj_warp
+			killAllTriggers
+			send "z*"
+			goto :twarp_adj
+		:locking
+			killAllTriggers
+			send "y"
+			setTextLineTrigger twarp_lock 		:twarp_lock "TransWarp Locked"
+			setTextLineTrigger no_twrp_lock 	:no_twarp_lock "No locating beam found"
+			setTextLineTrigger twarp_adj 		:twarp_adj "<Set NavPoint>"
+			setTextLineTrigger no_fuel 		:itwarpNoFuel "You do not have enough Fuel Ore"
+			pause
+		:twarpNoFuel
+			killAllTriggers
+			setVar $msg "Not enough fuel for T-warp."
+			goto :twarpDone
+
+		:twarp_adj
+			killAllTriggers
+			send " * p s"
+			goto :twarpDone
+
+		:twarpNoRoute
+			killAllTriggers
+			send "n* z* "
+			setVar $msg "No route available!"
+			goto :twarpDone
+
+		:no_twarp_lock
+			killAllTriggers
+			send "n*zn"
+			send "l " & #8 & $PLANET~PLANET "*c"
+			setSectorParameter $warpto "FIGSEC" FALSE
+			setvar $msg "no twarp lock"
+			return
+
+		:twarpIgd
+			killAllTriggers
+			setVar $msg "My ship is being held by Interdictor!"
+			goto :twarpDone
+
+		:twarpPhotoned
+			killAllTriggers
+			setVar $msg "I have been photoned and can not T-warp!"
+			goto :twarpDone
+
+		:twarp_lock
+			KillAlltriggers
+			if (currentalignment >= 1000)
+				if ($furbing)
+					setVar $str "y * * p s g y g q " 
+				else
+					setVar $str "y * *  " 
+				end
+				send $str
+			else
+				if ($furbing)
+					setVar $str "y  *  *  m " & $MAP~stardock & " *  *  p s g y g q "
+				else
+					setVar $str "y * *  " 
+				end
+				send $str
+			end
+		:twarpDone
+			if ($msg <> "")
+				setvar $switchboard~message "Twarp Error - " & $msg & "*"
+				gosub :switchboard~switchboard
+				send "*"
+			end
+	end
+	return
+
+:bwarp
+
+	killAllTriggers
+	send "b" $warpto "*"
+	setTextTrigger go :go5 "TransWarp Locked"
+	setTextTrigger no :no5 "No locating beam found"
+	goSub :delayTrigger
+	pause
+
+:no5
+	killAllTriggers
+	send "n "
+	waitfor "Transporter shutting down."
+	return
+
+:go5
+	killAllTriggers
+	send "y z * "
+	return
+
+:FindJumpSector
+	setVar $i 1
+	setVar $RED_adj 0
+	send "qq*"
+	while (SECTOR.WARPSIN[$MAP~stardock][$i] > 0)
+		setVar $RED_adj SECTOR.WARPSIN[$MAP~stardock][$i]
+		send "m " & $RED_adj & "* y"
+		setTextTrigger TwarpBlind 			:TwarpBlind "Do you want to make this jump blind? "
+		setTextTrigger TwarpLocked			:TwarpLocked "All Systems Ready, shall we engage? "
+		setTextLineTrigger TwarpVoided			:TwarpVoided "Danger Warning Overridden"
+		setTextLineTrigger TwarpAdj			:TwarpAdj "<Set NavPoint>"
+		pause
+		:TwarpAdj
+		killAllTriggers
+		send " * "
+		return
+
+		:TwarpVoided
+		killAllTriggers
+		send " N N "
+		goto :TryingNextAdj
+
+		:TwarpLocked
+		killAllTriggers
+		send " N "
+
+		goto :SectorLocked
+
+		:TwarpBlind
+		killAllTriggers
+		send " N "
+
+		:TryingNextAdj
+    	add $i 1
+	end
+
+	:NoAdjsFound
+		setVar $RED_adj 0
+		return
+
+	:SectorLocked
+		return
+
+
+:TurnsRequired
+	send "i"
+	setTextLineTrigger TurnsRequired_TPW	:TurnsRequired_TPW "Turns to Warp  : "
+	pause
+
+	:TurnsRequired_TPW
+	killAllTriggers
+	getWord CURRENTLINE $turnsRequired_TPW 5
+
+	if ($RED_adj > 0)
+		# twarp to jmp sector, then into SD sect, then twarp home
+		setVar $turnsRequired_temp ($turnsRequired_TPW * 3)
+		if ($_Tow > 0)
+			# 2 Turns for exporting into other ship and back again
+			add $turnsRequired_temp_temp 2
+			# 3 Turns for initial Port then x into other ship, port & shop, then x and report
+			#   b4 heading home
+			add $turnsRequired_temp 3
+		else
+			add $turnsRequired_temp 1
+		end
+	else
+		setVar $turnsRequired_temp ($turnsRequired_TPW * 2)
+		# 1 Turn to port at dock
+		add $turnsRequired_temp 1
+	end
+
+	setVar $turnsRequired $turnsRequired_temp
+	return
+
+
+:callSaveMe
+	send "q q q q * '"&$SWITCHBOARD~bot_name&" call*"
+	halt
+	
 #INCLUDES:
 include "source\module_includes\bot\loadvars\bot"
 include "source\module_includes\bot\helpfile\bot"
