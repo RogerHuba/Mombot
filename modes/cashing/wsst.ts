b471fa869dc360d186f75a7ff66ede7f25bbe8a0
Steven <steven@entropy.cc>
2020-04-04 23:58:05 -0500

    wppt2

--- modes/cashing/wsst.ts
+++ modes/cashing/wsst.ts
@@ -642,8 +642,175 @@ return
 			killtrigger 2
 
   return
-
 :refurb
+
+	setvar $twarp_refurb_success false
+	setVar $refurbPort $FURBING
+	if (($player~twarp_type <> "No") and ($refurbPort = $map~stardock))
+
+		gosub :twarprefurb
+		gosub :player~quikstats
+
+	end
+	if ($twarp_refurb_success <> true)
+		if ($FURBING <> 0)
+			setVar $mowIntoSector $FURBING
+			setVar $refurbPort $FURBING
+		else
+			setVar $mowIntoSector $refurbPort
+		end
+		if ($ultraSafe)
+			:trySafeMowAgainRefurb
+				gosub :safemowIntoSector
+				if ($isSafe = FALSE)
+					goto :trySafeMowAgainRefurb
+				end
+		else
+			gosub :mowIntoSector
+		end
+		gosub :player~quikstats
+		if ($player~current_sector = $refurbPort)
+			if ($FURBING <> $map~stardock)
+				send "p ty"
+				waitOn "A  Cargo holds     :"
+				getWord CURRENTLINE $holdsprice 5
+				getWord CURRENTLINE $holdsToBuy 10
+				setVar $beforeFurbCredits $player~credits
+				setVar $player~credits ($player~credits-($holdsprice * $holdsToBuy))
+				if ($player~credits > $CASH_TO_HOLD_ONTO)
+					if ($refurbFighters)
+						waitOn "B  Fighters        :"
+						getWord CURRENTLINE $figprice 4
+						getWord CURRENTLINE $figsToBuy 8
+					else
+						setVar $figsToBuy 0
+					end
+					if ($refurbShields)
+						waitOn "C  Shield Points   :"
+						getWord CURRENTLINE $shieldprice 5
+						getWord CURRENTLINE $player~shieldsToBuy 9
+					else
+						setVar $player~shieldsToBuy 0
+					end
+					if ($figsToBuy > 0)
+						if (($figprice * $figsToBuy) > ($player~credits-$CASH_TO_HOLD_ONTO))
+							setVar $figsToBuy (($player~credits-$CASH_TO_HOLD_ONTO)/$figprice)
+						end
+						setVar $player~credits ($player~credits-($figprice * $figsToBuy))
+					end
+					if ($player~shieldsToBuy > 0)
+						if (($shieldprice * $player~shieldsToBuy) > ($player~credits-$CASH_TO_HOLD_ONTO))
+							setVar $player~shieldsToBuy (($player~credits-$CASH_TO_HOLD_ONTO)/$shieldprice)
+						end
+						setVar $player~credits ($player~credits-($shieldprice * $player~shieldsToBuy))
+					end
+				else
+					setVar $figsToBuy 0
+					setVar $player~shieldsToBuy 0
+				end
+				send "a "&$holdsToBuy&"* y b "&$figsToBuy&"* c "&$player~shieldsToBuy&"* q q q z n * "
+				return
+			else
+				send "p s g y g q "
+			end
+		end
+	end
+
+	if ($player~current_sector = $refurbPort)
+		killAllTriggers
+		send " s p"
+		waitOn "A  Cargo holds     :"
+		getWord CURRENTLINE $holdsprice 5
+		getWord CURRENTLINE $holdsToBuy 10
+		setVar $beforeFurbCredits $player~credits
+		if ($player~credits > $CASH_TO_HOLD_ONTO)
+			if ($refurbFighters)
+				waitOn "B  Fighters        :"
+				getWord CURRENTLINE $figprice 4
+				getWord CURRENTLINE $figsToBuy 8
+			else
+				setVar $figsToBuy 0
+			end
+			if ($refurbShields)
+				waitOn "C  Shield Points   :"
+				getWord CURRENTLINE $shieldprice 5
+				getWord CURRENTLINE $player~shieldsToBuy 9
+			else
+				setVar $player~shieldsToBuy 0
+			end
+			if ($holdsToBuy > 0)
+				if (($holdsprice * $holdsToBuy) > ($player~credits-$CASH_TO_HOLD_ONTO))
+					setVar $holdsToBuy (($player~credits-$CASH_TO_HOLD_ONTO)/$holdsprice)
+				end
+				setVar $player~credits ($player~credits-($holdsprice * $holdsToBuy))
+			end
+			if ($figsToBuy > 0)
+				if (($figprice * $figsToBuy) > ($player~credits-$CASH_TO_HOLD_ONTO))
+					setVar $figsToBuy (($player~credits-$CASH_TO_HOLD_ONTO)/$figprice)
+				end
+				setVar $player~credits ($player~credits-($figprice * $figsToBuy))
+			end
+			if ($player~shieldsToBuy > 0)
+				if (($shieldprice * $player~shieldsToBuy) > ($player~credits-$CASH_TO_HOLD_ONTO))
+					setVar $player~shieldsToBuy (($player~credits-$CASH_TO_HOLD_ONTO)/$shieldprice)
+				end
+				setVar $player~credits ($player~credits-($shieldprice * $player~shieldsToBuy))
+			end
+		else
+			setVar $figsToBuy 0
+			setVar $player~shieldsToBuy 0
+			setvar $holdsToBuy 0
+		end
+			send "a "&$holdsToBuy&"* y b "&$figsToBuy&"* c "&$player~shieldsToBuy&"* q q h "
+			waitfor "<Hardware Emporium>"
+			if ($DROPLIMPS)
+				send "L"
+				waitfor "How many mines do you want"
+				getText CURRENTLINE $Buy "(Max" ") ["
+				striptext $buy " "
+				send $buy & "*"
+				waitfor "<Hardware Emporium>"
+			end
+			if ($DROPARMIDS)
+				send "M"
+				waitfor "How many mines do you want"
+				getText CURRENTLINE $Buy "(Max" ") ["
+				striptext $buy " "
+				send $buy & "*"
+				waitfor "<Hardware Emporium>"
+			end
+
+			send "/"
+			waitfor #179 & "Figs"
+			getText CURRENTLINE $player~credits (#179 & "Creds") (#179 & "Figs")
+			striptext $player~credits " "
+			stripText $player~credits ","
+
+		setVar $spentCredits ($spentCredits+($beforeFurbCredits-$player~credits))
+		setVar $player~fightersPurchased ($player~fightersPurchased+$figsToBuy)
+		setVar $player~shieldsPurchased ($player~shieldsPurchased+$player~shieldsToBuy)
+	else
+		send "'Something bad happened on refurb, I am probably in big trouble. [Temp error message until saveme implemented]*"
+	end
+	if ($twarp_refurb_success = true)
+		send "Q Q Q Q Z N M " & $START_SECTOR & "* Y  Y  Y  * *"
+		gosub :PLAYER~quikstats
+		if (player~current_sector = $MAP~stardock)
+			setvar $switchboard~message "Twarp Error, Should be Hiding on Dock!*"
+			gosub :switchboard~switchboard
+			send "*"
+			halt
+		end
+		send "jy*"
+
+	else
+		:donenormalfurb
+		setvar $twarp_refurb_success false
+		send " Q Q "
+	end	
+return
+
+:old_refurb
 
 	if ($FURBING <> 0)
 		setVar $mowIntoSector $FURBING
@@ -1267,6 +1434,200 @@ return
 	setArray $equipAtPort SECTORS
 	setArray $fuelAtPort SECTORS
 goto :GoGo
+
+
+:twarprefurb
+
+	# check adj's for Dock.. if present, then we don't need a jump sector.
+	setVar $i 1
+	setVar $START_SECTOR $player~current_sector
+	setVar $WeAreAdjDock FALSE
+	while ($i <= SECTOR.WARPCOUNT[$START_SECTOR])
+		setVar $adj_start SECTOR.WARPS[$START_SECTOR][$i]
+		if ($adj_start = $MAP~stardock)
+			setVar $WeAreAdjDock TRUE
+		end
+		add $i 1
+	end
+
+	Echo "**" & ANSI_14 & "Please Stand By" & ANSI_15 & " - Calculating Distances...**"
+	getdistance $dist1 $START_SECTOR $MAP~stardock
+
+	if ($dist1 <= 0)
+		setvar $switchboard~message "Insufficient Warp Data Plotting Course to Dock*"
+		gosub :switchboard~switchboard
+		send "*"
+		halt
+	end
+
+	getdistance $dist2 $MAP~stardock $START_SECTOR
+	if ($dist2 <= 0)
+		setvar $switchboard~message "Insufficient Warp Data Plotting Return Course From Dock*"
+		gosub :switchboard~switchboard
+		send "*"
+		halt
+	end
+
+	setVar $ore_req (($dist1 + $dist2) * 3)
+
+	if ($PLAYER~ORE_HOLDS < $ore_req)
+		setvar $switchboard~message "Not Enough ORE In Holds To Make Round Trip.  Needs "&$ore_req&".*"
+		gosub :switchboard~switchboard
+		send "*"
+		gosub :getsomefuel
+	end
+
+
+	if (($player~alignment < 1000) AND ($WeAreAdjDock = FALSE))
+		setVar $RED_adj 0
+		gosub :FindJumpSector
+		if ($RED_adj = 0)
+			waitfor "Command [TL="
+			setvar $switchboard~message "Cannot Find Jump Sector Adjacent Dock*"
+			gosub :switchboard~switchboard
+			send "*"
+			halt
+		end
+	end
+
+	if ($player~alignment >= 1000)
+		if ($WeAreAdjDock)
+			send "^F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+		else
+			send "^F" & $START_SECTOR & "*" & $MAP~stardock & "*F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+		end
+	else
+		if ($WeAreAdjDock)
+			send "^F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+		else
+			send "^F" & $START_SECTOR & "*" & $RED_adj & "*F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+		end
+	end
+	setTextLineTrigger noJoy :noJoy "*** Error - No route within"
+	setTextTrigger cont :cont "(?="
+	pause
+
+	:noJoy
+		killAllTriggers
+		setvar $switchboard~message "Cannot Find Path to StarDock!*"
+		gosub :switchboard~switchboard
+		send "*"
+		halt
+	:cont
+		killAllTriggers
+		setDelayTrigger Latency_Delay		:Latency_Delay 500
+		pause
+
+		:Latency_Delay
+
+
+		if ($PLAYER~TWARP_TYPE = "No")
+			setvar $switchboard~message "Must Have Twarp 1 or 2*"
+			gosub :switchboard~switchboard
+			send "*"
+			halt
+		end
+
+		if ($PLAYER~unlimitedGame = 0)
+			gosub :TurnsRequired
+			if ($turnsRequired > currentturns)
+				setvar $switchboard~message "Not Enough Turns. "&$turnsRequired&", Required*"
+				gosub :switchboard~switchboard
+				send "*"
+				halt
+			elseif ($turnsRequired <= currentturns)
+				setVar $tmp (currentturns - $turnsRequired)
+				if ($tmp <= $bot~bot_turn_limit)
+					setvar $switchboard~message "Proceeding Will Leave Fewer Than " & $bot~bot_turn_limit & " Turns!*"
+					gosub :switchboard~switchboard
+					send "*"
+					halt
+				end
+			end
+		end
+
+	send " C R " & $MAP~stardock & "*Q "
+	setTextLineTrigger itsalive :itsalive "Items     Status  Trading % of max OnBoard"
+	setTextLineTrigger nosoupforme :nosoupforme "I have no information about a port in that sector"
+	pause
+	:nosoupforme
+		killAllTriggers
+		setvar $switchboard~message "StarDock appears to have been Blown Up!*"
+		gosub :switchboard~switchboard
+		send "*"
+		halt
+	:itsalive
+		killAllTriggers
+		waitfor "(?="
+		setVar $msg ""
+		if ((currentalignment >= 1000) AND ($WeAreAdjDock = FALSE))
+			setVar $warpto $MAP~stardock
+			gosub :DoTwarp
+		elseif (($WeAreAdjDock = FALSE) AND ($RED_adj <> 0))
+			setVar $warpto $RED_adj
+			gosub :DoTwarp
+		else
+			send "q q *  m " & $MAP~stardock & "*  *  P  S G Y G Q "
+		end
+		if ($msg = "")
+			waitfor "You leave the Galactic Bank."
+		else
+			setvar $switchboard~message "Unknown Problem Detected. Check TA!*"
+			gosub :switchboard~switchboard
+			send "*"
+			halt
+		end
+		gosub :PLAYER~quikstats
+
+
+return
+
+
+:getsomefuel
+	gosub :player~quikstats
+	setVar $bottom 1
+	setVar $top 1
+	setArray $checked SECTORS
+	setVar $que[1] $player~current_sector
+	setVar $checked[$player~current_sector] 1
+	setvar $a 1
+	:try_again
+	while ($bottom <= $top)
+		# Now, pull out the next sector in the queue, and make it our focus
+		setVar $focus $que[$bottom]
+		getsectorparameter $focus "FIGSEC" $isFigged
+
+		if ((PORT.BUYFUEL[$focus] <> true) and (PORT.FUEL[$focus] > $player~total_holds))
+			setVar $mowintosector $focus
+			gosub :mowIntoSector
+			if (((PORT.BUYORG[$focus]) and ($player~organic_holds > 0)) OR ((PORT.BUYEQUIP[$focus]) and ($player~equipment_holds > 0)))
+				send "p t * * * * * * "
+			else
+				send "j y p t * * 0 * 0 * "
+			end
+			return
+		end
+		# That wasn't it, so let's add all the adjacents to the queue for future testing.
+		setVar $a 1
+		while (SECTOR.WARPS[$focus][$a] > 0)
+			setVar $adjacent SECTOR.WARPS[$focus][$a]
+			# But only add them if they haven't been added previously
+			if ($checked[$adjacent] = 0)
+				# Okay, this one hasn't been checked, so tag it and que it.
+				setVar $checked[$adjacent] 1
+				add $top 1
+				setVar $que[$top] $adjacent
+			end
+			add $a 1
+		end
+		# The adjacents of $focus were all queued, now on to the next one.
+		add $bottom 1
+	end	
+	setVar $SWITCHBOARD~message "Can't find a route to fuel.  Halting*"
+	gosub :SWITCHBOARD~switchboard
+	halt
+
+return
 
 
 #INCLUDES:
