ebb69fff3d0b9b159d647bdb8d219c0fb3c929c0
Steven <steven@entropy.cc>
2020-04-07 15:00:41 -0500

    Updating bot to 4.02

--- modes/data/fhit.ts
+++ modes/data/fhit.ts
@@ -426,7 +426,8 @@ return
 		add $x 1
 	end
 	if ($mode = "pdrop")
-		if ($foundSecs[$x][1] <> 0)
+		if ($foundSecs[$x][1] <> currentsector)
+			send "'About to drop on sector "&$foundSecs[$x][1]&" in "&$avgSec&" seconds..*"
 			setdelaytrigger waithere :nowdrop (($avgSec*1000)-500)
 			pause
 			:nowdrop
