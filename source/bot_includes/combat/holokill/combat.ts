d2c3fa8fa8f1bd8281e7cec5940911f4d351bbf2
Steven <steven@entropy.cc>
2020-04-08 20:23:34 -0500

    Updating bot to 4.02

--- source/bot_includes/combat/holokill/combat.ts
+++ source/bot_includes/combat/holokill/combat.ts
@@ -11,7 +11,7 @@
 	setvar $too_many_fighters (($ship~SHIP_OFFENSIVE_ODDS * $SHIP~SHIP_MAX_ATTACK))
 	divide $too_many_fighters 12
 
-	setTextLineTrigger noscan1 :holo_kill_noscanner "Handle which mine type, 1 Armid or 2 Limpet"
+	setTextTrigger noscan1 :holo_kill_noscanner "Handle which mine type, 1 Armid or 2 Limpet"
 	setTextLineTrigger noscan2 :holo_kill_noscanner "You don't have a long range scanner."
 	setTextLineTrigger scanned :holo_kill_scandone  "Warps to Sector(s) : "
 	if ($player~current_prompt = "Citadel")
@@ -20,6 +20,7 @@
 	else
 		send " sh"
 	end
+	send "'" rawpacket "*"
 	pause
 :holo_kill_noscanner
 		killalltriggers
