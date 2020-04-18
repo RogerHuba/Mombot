5845bca6a5bcce11b0e0365283d6d4f20ebd149c
Steven <steven@entropy.cc>
2020-04-18 15:47:48 -0500

    New changes

--- source/module_includes/deploy/mines.ts
+++ source/module_includes/deploy/mines.ts
@@ -61,6 +61,7 @@
 	end
 
 	send $start_mac "z n h 2 z " $amount "*  z" $mine " h 1 z " $amount "*  z " $mine $end_mac
+	waiton "Warps to Sector(s) :"
 	gosub :PLAYER~quikstats
 	
 	if ((($predeployArmids > $player~armids) AND ($predeployLimpets > $player~limpets)) OR (($predeployLimpets = $player~limpets) AND (($limpetOwner = "belong to your Corp") OR ($limpetOwner = "yours")) AND ($predeployArmids = $player~armids) AND (($armidOwner = "belong to your Corp") OR ($armidOwner = "yours"))))
