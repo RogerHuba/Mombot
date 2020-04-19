1e1e7099352acf487d20972006261ebf1aaa00ff
Steven <steven@entropy.cc>
2020-04-19 11:15:15 -0500

    new k and m logic for numbers

--- source/public/TWXP/scripts/mombot4p/commands/general/relog.ts
+++ source/public/TWXP/scripts/mombot4p/commands/general/relog.ts
@@ -49,7 +49,7 @@
 			send $BOT~password & "* **"
 			settextlinetrigger dead :dead "What do you want to name your ship? (30 letters)"
 			settexttrigger alive :alldone_relog "Command ["
-			settextlinetrigger avoids :continueavoids "<Scanning for Hazardous Sectors you have marked to Avoid>"
+			settexttrigger avoids :continueavoids "Do you wish to clear some avoids? (Y/N) [N]"
 			settexttrigger messages :continuemessages "[Pause]"
 			settexttrigger delete :continuedelete "[Pause] - Delete messages? (Y/N)"
 			pause
