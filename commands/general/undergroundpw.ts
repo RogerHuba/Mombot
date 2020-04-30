468f7f79395af4d67dc080be0bc5cebd0a52e550
Steven <steven@entropy.cc>
2020-04-30 00:29:52 -0500

    Updating mooexp

---
+++ commands/general/undergroundpw.ts
@@ -0,0 +1,16 @@
+	gosub :BOT~loadVars
+
+
+	setVar $BOT~help[1]  $BOT~tab&"password "
+	setVar $BOT~help[2]  $BOT~tab&"  Sends ansi password"
+	setVar $BOT~help[3]  $BOT~tab&"      "
+	gosub :bot~helpfile
+
+
+	send #127 #184 #156 "*"
+halt
+	
+#INCLUDES:
+include "source\module_includes\bot\loadvars\bot"
+include "source\module_includes\bot\helpfile\bot"
+
