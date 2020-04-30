094eb407509dd0fab7432d7205c5b7d5de3ffc07
Steven <steven@entropy.cc>
2020-04-30 01:17:22 -0500

    Updating mooexp

--- commands/general/undergroundpw.ts
+++ commands/general/undergroundpw.ts
@@ -6,9 +6,38 @@
 	setVar $BOT~help[3]  $BOT~tab&"      "
 	gosub :bot~helpfile
 
-	send  "test" ANSI_10 "test"
+	send  "test" 
 
-
+	send #1
+    send #2
+    send #3
+    send #4
+    send #5
+    send #6
+    send #7
+    send #8
+    send #9
+    send #11
+    send #12
+    send #14
+    send #15
+    send #16
+    send #17
+    send #18
+    send #19
+    send #20
+    send #21
+    send #22
+    send #23
+    send #24
+    send #25
+    send #26
+    send #27
+    send #28
+    send #29
+    send #30
+    send #31
+ 
 halt
 	
 #INCLUDES:
