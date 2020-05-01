c46c5acfab3cec8c8030243f7d70e1478ccc43e1
Steven <steven@entropy.cc>
2020-05-01 01:00:58 -0500

    update

--- modes/cashing/mooexp.ts
+++ modes/cashing/mooexp.ts
@@ -2309,6 +2309,12 @@ return
 				goSub :smallDelay
 				send "uy"
 				goSub :smallDelay
+			gosub :player~quikstats
+			if (($player~current_prompt <> "Planet") and ($player~current_prompt <> "Citadel"))
+				setvar $switchboard~message "Something went wrong - should be at planet prompt.*"
+				gosub :switchboard~switchboard
+				pause
+			end				
 			setVar $BOT~command "pimp"
             setVar $BOT~user_command_line #34& "m185380721" &#34& " f "
             setVar $BOT~parm1 "m185380721"
