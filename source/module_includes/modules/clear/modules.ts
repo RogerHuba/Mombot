506573ab02acd84acceab511870064b457ae8666
Steven <steven@entropy.cc>
2020-05-06 13:31:09 -0500

    Updating ugrid

--- source/module_includes/modules/clear/modules.ts
+++ source/module_includes/modules/clear/modules.ts
@@ -59,7 +59,7 @@
 		gosub :clear_sector_deployEquipment
 		return
 	:clear_sector_xenter
-		send "q y n * t* * *" $bot~password "*    *    *       za9999*   z*   "
+		send "q y n * t* * *" $bot~password "*    *    *       za9999*   z*   * "
 		return
 	:clear_sector_deployEquipment
 		if ($player~surroundmine <= 0)
@@ -84,10 +84,10 @@
 		end
 		setVar $clearMac ""
 		if (($armidOwner <> "belong to your Corp") AND ($armidOwner <> "yours"))
-			setVar $clearMac $clearMac&"h  1  z " & $minesToDeploy & "*  z c  *  "
+			setVar $clearMac $clearMac&"h  1  z " & $minesToDeploy & "*  z c  *  * "
 		end
 		if (($limpetOwner <> "belong to your Corp") AND ($limpetOwner <> "yours"))
-			setVar $clearMac $clearMac&"h  2  z " & $limpsToDeploy & "*  z c  *   "
+			setVar $clearMac $clearMac&"h  2  z " & $limpsToDeploy & "*  z c  *  *  "
 		end
 		send $clearMac
 		gosub :PLAYER~quikstats
