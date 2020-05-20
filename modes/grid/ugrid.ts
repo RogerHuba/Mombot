87e3172ab3d747ad7d6ceb2c1fdbd781aae130a4
Steven <steven@entropy.cc>
2020-05-20 17:58:48 -0500

    Updating changes for mombot

--- modes/grid/ugrid.ts
+++ modes/grid/ugrid.ts
@@ -1206,10 +1206,8 @@ return
 	setvar $photoned false
 	if ($player~warpto > 0)
 		if ($furbing = true)
-			echo "**1[" $furbing "]**"
 			send "mz" & $player~warpto "*"
 		else
-			echo "**2[" $furbing "]**"
 			send "q  q  mz" & $player~warpto "*"
 		end
 		setTextTrigger there        :adj_warp "You are already in that sector!"
