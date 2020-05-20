4d13c4dc446aad5b36aa2edae28ae87ec1e42b05
Steven <steven@entropy.cc>
2020-05-20 16:55:52 -0500

    Updating changes for mombot

--- source/bot_includes/bot/internal_commands.ts
+++ source/bot_includes/bot/internal_commands.ts
@@ -180,10 +180,6 @@ goto :BOT~wait_for_command
 goto :BOT~wait_for_command
 #=========================== END PHOTON HOTKEY =======================================
 
-:clear
-	setVar $BOT~user_command_line "clear"
-	goto :modules~clear
-
 :kit
 	setVar $BOT~user_command_line "macro_kit"
 	goto :USER_INTERFACE~runUserCommandLine
