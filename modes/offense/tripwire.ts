bd2059d2680d68016266b613341ada79f730f191
unknown <steven.p.ruzicka@gmail.com>
2020-11-14 13:36:55 -0600

    dockshopper

--- modes/offense/tripwire.ts
+++ modes/offense/tripwire.ts
@@ -193,11 +193,11 @@
 
 		setvar $macro "q "
 		if (($planet~PLANET_FUEL_COLONISTS > 0) or ($planet~PLANET_ORGANICS_COLONISTS > 0) or ($planet~PLANET_EQUIPMENT_COLONISTS > 0))
-			setvar $macro "q z a y 99999* z a y 99999* "
+			setvar $macro "z a y 99999* z a y 99999* z a y 99999* "
 			setVar $SWITCHBOARD~message "There are colonists on the planet.  Will attempt to kill them, but you can make the macro faster buy killing now, and running tripwire again.*"
 			gosub :SWITCHBOARD~switchboard
 		end
-		setvar $macro $macro&"z a y 99999* z d y j a y j 99999* j a y j 99999* j a y j 99999* j a y j 99999* j a y j 99999**  "
+		setvar $macro $macro&"z d y j a y j 99999* j a y j 99999* j a y j 99999* j a y j 99999* j a y j 99999**  "
 		setTextLineTrigger time_to_blow :check " invaded and captured "&$planet~planet_NAME
 		setTextLineTrigger time_to_blow_2 :check_for_blow " of your fighters on planet "&$planet~planet_NAME
 		setTextLineTrigger time_to_blow_3 :check_for_blow_2 " of your Planetary Shields on planet "&$planet~planet_NAME
