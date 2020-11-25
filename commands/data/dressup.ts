bf0cd910e7fd1f76f61b044b1d7646271d001d43
unknown <steven.p.ruzicka@gmail.com>
2020-11-25 14:17:37 -0600

    dockshopper

--- commands/data/dressup.ts
+++ commands/data/dressup.ts
@@ -1,11 +1,3 @@
-	gosub :BOT~loadVars
-
-setvar $switchboard~message "Adding costumes..*"
-gosub :switchboard~switchboard
 
 AddQuickText "Werewolf" "Mind Dagger"
 AddQuickText "Minotaur" "Hammer"
-
-#INCLUDES:
-include "source\module_includes\bot\loadvars\bot"
-include "source\module_includes\bot\helpfile\bot"
