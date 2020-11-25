49b46b9d06c9f4aa036b91c3a3745695f41c3f27
unknown <steven.p.ruzicka@gmail.com>
2020-11-25 14:16:27 -0600

    dockshopper

--- commands/data/dressup.ts
+++ commands/data/dressup.ts
@@ -1,13 +1,10 @@
 	gosub :BOT~loadVars
 
-	setVar $BOT~help[1] $BOT~tab&"dressup {original text} {new text}"
-	setVar $BOT~help[2] $BOT~tab&"    displays all busted sectors on subspace"
-	gosub :bot~helpfile
-
 setvar $switchboard~message "Adding costumes..*"
 gosub :switchboard~switchboard
 
 AddQuickText "Werewolf" "Mind Dagger"
+AddQuickText "Minotaur" "Hammer"
 
 #INCLUDES:
 include "source\module_includes\bot\loadvars\bot"
