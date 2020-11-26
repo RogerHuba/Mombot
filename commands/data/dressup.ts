bce0a6c40fcd85880c466acbf7f6b359f7cb8df5
unknown <steven.p.ruzicka@gmail.com>
2020-11-25 19:24:06 -0600

    dockshopper

--- commands/data/dressup.ts
+++ commands/data/dressup.ts
@@ -1,3 +1,7 @@
 
 AddQuickText "Werewolf" "Mind Dagger"
 AddQuickText "Minotaur" "Hammer"
+AddQuickText "Zombie" "Kane"
+AddQuickText "Headless Horseman" "General Payne"
+AddQuickText "Igor" "Ohmz"
+AddQuickText "Bigfoot" "Sando"
