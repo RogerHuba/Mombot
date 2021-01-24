ee0e16ccf81e5803bf9c6d354ca289df9c409bbd
unknown <steven.p.ruzicka@gmail.com>
2021-01-24 02:55:03 -0600

    Updating defender

--- source/module_includes/bot/helpfile/bot.ts
+++ source/module_includes/bot/helpfile/bot.ts
@@ -101,7 +101,7 @@ end
 									###########################################
 									getwordpos $rest_of_string $pos "}"
 									send "'"&$rest_of_string&"*"
-									cuttext $rest_of_string $rest_of_string $pos 9999
+									cuttext $rest_of_string $rest_of_string ($pos+1) 9999
 
 									replacetext $option "{" ""
 									replacetext $option "}" ""
