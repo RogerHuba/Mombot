2af642ff0ed6d8d91a47e481e44f42ce5c69064a
unknown <steven.p.ruzicka@gmail.com>
2021-02-02 14:50:07 -0600

    update

--- source/bot_includes/bot.ts
+++ source/bot_includes/bot.ts
@@ -71,10 +71,9 @@ return
 	SetTextOutTrigger   UpArrow2    :USER_INTERFACE~User_Access    #27&"[A"
 	SetTextOutTrigger   DownArrow2  :USER_INTERFACE~User_Access    #27&"[B"
 	SetTextOutTrigger   Tabkey      :USER_INTERFACE~Hotkey_Access  #9
-	SetTextOutTrigger   UpArrow3    :USER_INTERFACE~User_Access    (#27&#91&#53&#126)
-	SetTextOutTrigger   DownArrow3  :USER_INTERFACE~User_Access    (#27&#91&#54&#126)
-	SetTextOutTrigger   RightArrow  :USER_INTERFACE~Hotkey_Access  #27&#91
-#	SetTextOutTrigger   LeftArrow2  :USER_INTERFACE~Hotkey_Access  #30
+	SetTextOutTrigger   Pageup      :USER_INTERFACE~User_Access    (#27&#91&#53&#126)
+	SetTextOutTrigger   Pagedown    :USER_INTERFACE~User_Access    (#27&#91&#54&#126)
+	SetTextOutTrigger   escapekey   :USER_INTERFACE~Hotkey_Access  #27
 
 	setVar $USER_INTERFACE~authorization 0
 	setVar $USER_INTERFACE~logged 0
