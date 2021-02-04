c754964f177e1c3f8691ac0fc842831e92d68907
unknown <steven.p.ruzicka@gmail.com>
2021-02-04 10:18:22 -0600

    installer updated

--- source/module_includes/bot/helpfile/bot.ts
+++ source/module_includes/bot/helpfile/bot.ts
@@ -190,7 +190,7 @@ end
 				setvar $i 1
 				addMenu "" "MENUSYSTEM" $command&" Settings" "." "" "Main" FALSE
 				while ($i <= $fields)
-					addMenu "MENUSYSTEM" $fields[$i] $fields[$i][3] $i ":"&$command&$i "" FALSE
+					addMenu "MENUSYSTEM" $fields[$i] $fields[$i][3] $i ":"&$command&$i $fields[$i][3] FALSE
 					
 					add $i 1
 				end
