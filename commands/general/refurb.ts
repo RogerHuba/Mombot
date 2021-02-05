a39cfa04012966e282323b06351ee159948a6ab4
unknown <steven.p.ruzicka@gmail.com>
2021-02-05 15:15:20 -0600

    installer updated

--- commands/general/refurb.ts
+++ commands/general/refurb.ts
@@ -1,17 +1,53 @@
 gosub :BOT~loadVars
 	setvar $bot~command "refurb"
 
-	setVar $BOT~help[1]  $BOT~tab&"refurb {seek} "
+	setVar $BOT~help[1]  $BOT~tab&"refurb {holds} {fighters} {shields} {seek} "
 	setVar $BOT~help[2]  $BOT~tab&"     "
 	setVar $BOT~help[3]  $BOT~tab&"   Auto buys fighters and shields"
 	setVar $BOT~help[4]  $BOT~tab&"     "
-	setVar $BOT~help[5]  $BOT~tab&"   {seek} - twarp to class 9 or 0 port and back"
+	setVar $BOT~help[5]  $BOT~tab&"       {seek} - twarp to class 9 or 0 port and back"
+	setVar $BOT~help[6]  $BOT~tab&"      {holds} - buy holds"
+	setVar $BOT~help[7]  $BOT~tab&"   {fighters} - buy fighters"
+	setVar $BOT~help[8]  $BOT~tab&"    {shields} - buy shields"
 	gosub :bot~helpfile
 
 	setVar $message ""
 	setVar $BOT~validPrompts "Citadel Command"
 	gosub :BOT~checkStartingPrompt
 	setVar $startingLocation $PLAYER~CURRENT_PROMPT
+
+
+	getwordpos " "$bot~user_command_line&" " $pos " seek "
+	setvar $seek false
+	if ($pos > 0)
+		setvar $seek true
+	end
+
+	getwordpos " "$bot~user_command_line&" " $pos " h"
+	setvar $holds false
+	if ($pos > 0)
+		setvar $holds true
+	end
+
+	getwordpos " "$bot~user_command_line&" " $pos " f"
+	setvar $fighters false
+	if ($pos > 0)
+		setvar $fighters true
+	end
+
+	getwordpos " "$bot~user_command_line&" " $pos " sh"
+	setvar $shields false
+	if ($pos > 0)
+		setvar $shields true
+	end
+
+	if (($shields <> true) and ($fighters <> true) and ($holds <> true))
+		# default is all are true #
+		setvar $holds true
+		setvar $fighters true
+		setvar $shields true
+	end
+
 	if ((CURRENTSECTOR = 1) OR (PORT.CLASS[CURRENTSECTOR] = 0) or (CURRENTSECTOR = $map~rylos) or (CURRENTSECTOR = $map~alpha_centauri))
 		if ($startingLocation = "Citadel")
 			send "q t*t1* "
@@ -22,7 +58,7 @@ gosub :BOT~loadVars
 	elseif (CURRENTSECTOR = $MAP~STARDOCK)
 		send "p ss ys *p"
 	else
-		if ($BOT~parm1 = "seek")
+		if ($seek = true)
 			if ($startingLocation = "Citadel")
 				send "q t*t1* "
 				gosub :PLANET~getPlanetInfo
@@ -84,7 +120,16 @@ gosub :BOT~loadVars
 			getWord CURRENTLINE $figsToBuy 8
 			waitOn " credits per point "
 			getWord CURRENTLINE $shieldsToBuy 9
-			send "a "&$holdsToBuy&"* b "&$figsToBuy&"* c "&$shieldsToBuy&"* q q q * "
+			if ($holds = true)
+				send "a "&$holdsToBuy&"* "
+			end
+			if ($fighters = true)
+				send "b "&$figsToBuy&"* "
+			end
+			if ($shields = true)
+				send "c "&$shieldsToBuy&"* "
+			end
+			send "q q q * "
 		else
 			send "b 0* c 0* q q q * "
 		end
