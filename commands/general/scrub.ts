234a1e6ef36432e9974cf54800bc6a57e3cbf55f
unknown <steven.p.ruzicka@gmail.com>
2021-02-05 13:48:50 -0600

    installer updated

--- commands/general/scrub.ts
+++ commands/general/scrub.ts
@@ -48,7 +48,7 @@ end
 				gosub :player~twarp
 				gosub  :player~currentPrompt
 				if ($PLAYER~twarpSuccess = TRUE)
-					send "P  S G Y G Q s p"
+					send "P  S G YG Q s p"
 				else
 					setVar $SWITCHBOARD~message $PLAYER~msg&"*"
 					gosub :SWITCHBOARD~switchboard
