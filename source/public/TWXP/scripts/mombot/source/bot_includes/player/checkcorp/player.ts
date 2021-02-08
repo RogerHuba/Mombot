ce28549c2e4cadf300cee5ad30357dd7e7357b4b
Steven Ruzicka <steven@entropy.cc>
2021-02-08 01:44:13 -0600

    public

--- source/public/TWXP/scripts/mombot/source/bot_includes/player/checkcorp/player.ts
+++ source/public/TWXP/scripts/mombot/source/bot_includes/player/checkcorp/player.ts
@@ -16,7 +16,8 @@
 
 		:ta_check
 			getwordpos CURRENTLINE $pos "P indicates Trader is on a planet in that sector"
-			if ($pos > 0)
+			getwordpos CURRENTLINE $pos2 "Corporate command ["
+			if (($pos > 0) or ($pos2 > 0))
 				goto :done_ta
 			end
 			setvar $line CURRENTLINE
@@ -31,5 +32,4 @@
 
 	:done_ta
 	send "q"
-	waiton "Citadel command ("
 return
