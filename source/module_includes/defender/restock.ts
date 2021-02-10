6767150e40f0c433f2926bece2006aaab7d3ad2b
unknown <steven.p.ruzicka@gmail.com>
2021-02-09 19:10:55 -0600

    install

--- source/module_includes/defender/restock.ts
+++ source/module_includes/defender/restock.ts
@@ -137,7 +137,7 @@
 		end
 		setVar $cashNeeded ($photonCashNeeded+$limpetCashNeeded+$armidCashNeeded+$disruptorCashNeeded+$genesisCashNeeded+$game~LIMPET_REMOVAL_COST)
 		setVar $furbing TRUE
-		if ($cashNeeded > currentcredits)
+		if ($cashNeeded > $player~credits)
 			send "D" 
 			waitOn "Citadel treasury contains "
 			getWord CURRENTLINE $citadelCash 4
@@ -147,11 +147,11 @@
 				gosub :switchboard~switchboard
 				gosub :navigate~head_home
 			end
-			send "t f "&($cashNeeded-currentcredits)&"* "
+			send "t f "&($cashNeeded-$player~credits)&"* "
 		end
 		# check adj's for Dock.. if present, then we don't need a jump sector.
 		setVar $i 1
-		setVar $START_SECTOR currentsector
+		setVar $START_SECTOR $player~current_sector
 		setVar $WeAreAdjDock FALSE
 		while ($i <= SECTOR.WARPCOUNT[$START_SECTOR])
 			setVar $adj_start SECTOR.WARPSIN[$START_SECTOR][$i]
@@ -183,17 +183,41 @@
 			getdistance $dist2 $player~RED_adj $START_SECTOR
 		end
 		if (($dist1 < 0) or $dist2 < 0)
-			if (currentalignment >= 1000)
-				if ($WeAreAdjDock)
-					send "^F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+			if ($player~alignment >= 1000)
+				if ($WeAreAdjDock = true)
+					setvar $player~starting_point $map~stardock
+					setvar $player~destination $start_sector
+					gosub :player~getcourse
+					setvar $dist1 1
+					setvar $dist2 $player~courseLength
 				else
-					send "^F" & $START_SECTOR & "*" & $MAP~stardock & "*F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+					setvar $player~starting_point $start_sector
+					setvar $player~destination $map~stardock
+					gosub :player~getcourse
+					setvar $dist1 $player~courseLength
+
+					setvar $player~starting_point $map~stardock
+					setvar $player~destination $start_sector
+					gosub :player~getcourse
+					setvar $dist2 $player~courseLength
 				end
 			else
-				if ($WeAreAdjDock)
-					send "^F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+				if ($WeAreAdjDock = true)
+					setvar $player~starting_point $map~stardock
+					setvar $player~destination $start_sector
+					gosub :player~getcourse
+					setvar $dist1 1
+					setvar $dist2 $player~courseLength
 				else
-					send "^F" & $START_SECTOR & "*" & $player~RED_adj & "*F" & $MAP~stardock & "*" & $START_SECTOR & "*Q/ "
+					setvar $player~starting_point $start_sector
+					setvar $player~destination $player~RED_adj
+					gosub :player~getcourse
+					setvar $dist1 $player~courseLength
+
+					setvar $player~starting_point $map~stardock
+					setvar $player~destination $start_sector
+					gosub :player~getcourse
+					setvar $dist2 $player~courseLength
 				end
 			end
 			setTextLineTrigger noJoy :noJoy "*** Error - No route within"
@@ -266,13 +290,13 @@
 
 			if ($PLAYER~unlimitedGame = 0)
 				gosub :TurnsRequired
-				if ($turnsRequired > currentturns)
+				if ($turnsRequired > $player~turns)
 					setvar $switchboard~message "Not Enough Turns. "&$turnsRequired&", Required*"
 					gosub :switchboard~switchboard
 					send "*"
 					return
-				elseif ($turnsRequired <= currentturns)
-					setVar $tmp (currentturns - $turnsRequired)
+				elseif ($turnsRequired <= $player~turns)
+					setVar $tmp ($player~turns - $turnsRequired)
 					if ($tmp <= $bot~bot_turn_limit)
 						setvar $switchboard~message "Proceeding Will Leave Fewer Than " & $bot~bot_turn_limit & " Turns!*"
 						gosub :switchboard~switchboard
