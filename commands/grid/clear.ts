fb48fbd74e1718d2ed51a674a9066a230390df45
unknown <steven.p.ruzicka@gmail.com>
2021-02-10 22:37:13 -0600

    install

--- commands/grid/clear.ts
+++ commands/grid/clear.ts
@@ -19,4 +19,5 @@
 # includes:
 include "source\module_includes\bot\loadvars\bot"
 include "source\module_includes\bot\helpfile\bot"
+include "source\module_includes\bot\player\quikstats\player"
 include "source\module_includes\modules\clear\modules"
