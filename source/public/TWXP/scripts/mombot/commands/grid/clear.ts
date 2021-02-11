091617b93eae28b0c887651b4814314406e526e9
Steven Ruzicka <steven@entropy.cc>
2021-02-10 23:04:54 -0600

    fix

--- source/public/TWXP/scripts/mombot/commands/grid/clear.ts
+++ source/public/TWXP/scripts/mombot/commands/grid/clear.ts
@@ -19,6 +19,6 @@
 # includes:
 include "source\module_includes\bot\loadvars\bot"
 include "source\module_includes\bot\helpfile\bot"
-include "source\module_includes\bot\getstartingprompt\bot"
+include "source\module_includes\bot\checkstartingprompt\bot"
 include "source\bot_includes\player\quikstats\player"
 include "source\module_includes\modules\clear\modules"
