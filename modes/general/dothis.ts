78b055ccaa53ebbc5a28ee51cd5794a4d2d9c23f
unknown <steven.p.ruzicka@gmail.com>
2021-02-12 23:31:05 -0600

    merge

--- modes/general/dothis.ts
+++ modes/general/dothis.ts
@@ -204,6 +204,89 @@
 	gosub :MAIN~backwards_compatible
 return
 
+
+:MAIN~backwards_compatible
+	setVar  $safe_ship $bot~safe_ship
+	saveVar $safe_ship
+	setVar  $safe_planet $bot~safe_planet
+	saveVar $safe_planet
+	setVar $command $bot~command
+	saveVar $command
+	setvar $user_command_line $bot~user_command_line
+	saveVar $user_command_line
+	setVar $bot_name $bot~bot_name
+	saveVar $bot_name
+	setVar $self_command $bot~self_command
+	saveVar $self_command
+	setvar $parm1 $bot~parm1
+	setvar $parm2 $bot~parm2
+	setvar $parm3 $bot~parm3
+	setvar $parm4 $bot~parm4
+	setvar $parm5 $bot~parm5
+	setvar $parm6 $bot~parm6
+	setvar $parm7 $bot~parm7
+	setvar $parm8 $bot~parm8
+	if ($parm1 = "")
+		setvar $parm1 "0"
+	end
+	if ($parm2 = "")
+		setvar $parm2 "0"
+	end
+	if ($parm3 = "")
+		setvar $parm3 "0"
+	end
+	if ($parm4 = "")
+		setvar $parm4 "0"
+	end
+	if ($parm5 = "")
+		setvar $parm5 "0"
+	end
+	if ($parm6 = "")
+		setvar $parm6 "0"
+	end
+	if ($parm7 = "")
+		setvar $parm7 "0"
+	end
+	if ($parm8 = "")
+		setvar $parm8 "0"
+	end
+	saveVar $parm1
+	saveVar $parm2
+	saveVar $parm3
+	saveVar $parm4
+	saveVar $parm5
+	saveVar $parm6
+	saveVar $parm7
+	saveVar $parm8
+	setVar $rylos $map~rylos
+	saveVar $rylos
+	setVar $alpha_centauri $map~alpha_centauri
+	saveVar $alpha_centauri
+	setVar $stardock $map~stardock
+	saveVar $stardock
+	setVar $backdoor $map~backdoor
+	saveVar $backdoor
+	setVar $home_sector $map~home_sector
+	saveVar $home_sector
+	setVar $alarm_list $bot~alarm_list
+	saveVar $alarm_list
+	setVar $unlimitedGame $player~unlimitedGame
+	saveVar $unlimitedGame
+	setVar $bot_turn_limit $bot~bot_turn_limit
+	saveVar $bot_turn_limit
+	setVar $steal_factor $game~steal_factor
+	saveVar $steal_factor
+	setVar $password $bot~password
+	saveVar $password
+	setVar $mode $bot~mode
+	saveVar $mode
+	setVar $subspace $bot~subspace
+	saveVar $subspace
+	setVar $ptradesetting $game~ptradesetting
+	saveVar $ptradesetting
+
+return
+
 #-=-=-=-=-includes-=-=-=-=-
 include "source\module_includes\bot\loadvars\bot"
 include "source\module_includes\bot\helpfile\bot"
