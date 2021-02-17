36977df4ebb48a1f30a7742825b3a0587af694c6
unknown <steven.p.ruzicka@gmail.com>
2021-02-16 23:23:55 -0600

    update

--- mombot.ts
+++ mombot.ts
@@ -7,7 +7,7 @@ reqRecording
 # Credits           : Oz, Zentock, SupG, Dynarri, Cherokee, Alexio, Xide, Phx, Rincrast, Voltron, Traitor, Parrothead, PSI, Elder Prophet, Caretaker, Deign
 
 setVar $bot~major_version   "4"
-setVar $bot~minor_version   "61beta"
+setVar $bot~minor_version   "7beta"
 setvar $bot~default_bot_directory "mombot"
 
 savevar $bot~major_version
