24f3b471bd99bcf38b8e13f9ca60b7aaf6820b93
unknown <steven.p.ruzicka@gmail.com>
2021-02-23 23:22:06 -0600

    merge

--- commands/data/history.ts
+++ commands/data/history.ts
@@ -1,12 +1,18 @@
 	logging off
 	gosub :BOT~loadVars
 
-	setVar $BOT~help[1]  $BOT~tab&" history {limit:#}"
+	setVar $BOT~help[1]  $BOT~tab&" history {limit:#} {filter:x}"
 	setVar $BOT~help[2]  $BOT~tab&"   "
 	setVar $BOT~help[3]  $BOT~tab&"     Displays the most recent self commands"
 	setVar $BOT~help[4]  $BOT~tab&"     this bot has given."
 	setVar $BOT~help[5]  $BOT~tab&"         "
-	setVar $BOT~help[6]  $BOT~tab&"     {limit#} - display the last # of commands"
+	setVar $BOT~help[6]  $BOT~tab&"      {limit:#} - display the last # of commands"
+	setVar $BOT~help[7]  $BOT~tab&"     {filter:x} - only show commands matching this"
+	setVar $BOT~help[8]  $BOT~tab&"                      "
+	setVar $BOT~help[9]  $BOT~tab&"     Example:                      "
+	setVar $BOT~help[10] $BOT~tab&"         >history 10                      "
+	setVar $BOT~help[11] $BOT~tab&"         >history pdrop                   "
+	setVar $BOT~help[12] $BOT~tab&"         >history 3 pdrop                   "
 
 	gosub :bot~helpfile
 
@@ -49,12 +55,14 @@
 			subtract $i 1
 		end
 	else
-		setvar $i $history_limit
+		setvar $count 0
+		setvar $i $BOT~historyCount
 		setvar $switchboard~message $switchboard~message&"Displaying last commands matching ["&$filter&"]:*"
-		while ($i >= 1)
+		while (($i >= 1) and ($count >= $history_limit))
 			if ($history[($i+$place)] <> "0")
 				getwordpos $history[($i+$place)] $pos $filter 
 				if ($pos > 0)
+					add $count 1
 					setvar $switchboard~message $switchboard~message&$history[($i+$place)]&"*"
 				end
 			end
