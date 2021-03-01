6d58500091770c1cb4fae3136103c76a071e86bb
unknown <steven.p.ruzicka@gmail.com>
2021-02-28 20:14:32 -0600

    merge

--- modes/resource/upgrader.ts
+++ modes/resource/upgrader.ts
@@ -65,7 +65,7 @@ logging off
 
 getWordPos " "&$bot~user_command_line&" " $pos "ignore:"
 if ($pos > 0)
-	getText " "&$bot~user_command_line&" " $MassUpgrade~ignore_list "ignore:" " "
+	getText " "&$bot~user_command_line&" " $MassUpgrade~IgnoreList "ignore:" " "
 	replacetext $MassUpgrade~IgnoreList "," " "
 else
 	setVar $MassUpgrade~IgnoreList ""
