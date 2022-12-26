76f484925846ee5486685a5cd0065094350fe02c
Steven Ruzicka <stevenruzicka@stevens-mbp.lan>
2022-12-26 14:33:02 -0600

    adding logic to mow to check for invalidsector

--- modes/grid/mow.ts
+++ modes/grid/mow.ts
@@ -52,9 +52,7 @@
 	setVar $bot~startingLocation $PLAYER~CURRENT_PROMPT
 	setVar $bot~validPrompts "Command <Underground> Do How Corporate Citadel Planet Computer Terra <StarDock> <FedPolice> <Tavern> <Libram <Galactic <Hardware <Shipyards>"
 	gosub :bot~checkStartingPrompt
-	echo $bot~parm1&"*"
-	gosub :player~checkfortravelname
-	echo $bot~parm1&"*"
+#	gosub :player~checkfortravelname
 
 		setVar $PLAYER~destination $bot~parm1
 		isNumber $number $PLAYER~destination
