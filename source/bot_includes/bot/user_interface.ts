4c556391471da0e1c38ad5167b7ec3d5dda45d14
Steven Ruzicka <stevenruzicka@stevens-mbp.lan>
2022-12-30 14:25:30 -0600

    reverting bot

--- source/bot_includes/bot/user_interface.ts
+++ source/bot_includes/bot/user_interface.ts
@@ -237,7 +237,7 @@ return
 				if ($test_value <> 0)
 					replaceText $bot~command_lines[$b] $test_value&"k" ($test_value*1000)
 					replaceText $bot~command_lines[$b] $test_value&"m" ($test_value*1000000)
-					replaceText $bot~command_lines[$b] $test_value&"b" ($test_value*000000000)
+					replaceText $bot~command_lines[$b] $test_value&"b" ($test_value*1000000000)
 				end
 			end
 		end
